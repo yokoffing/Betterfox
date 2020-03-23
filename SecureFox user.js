9aa9eaeab990eb954452dae0cbb48d3e213f499b
hjstephens09 <hjstephens09@gmail.com>
2020-03-23 19:06:33 -0400

    Add files via upload

---
+++ SecureFox user.js
@@ -0,0 +1,196 @@
+/******
+
+* name: SecureFox
+* description: Immensely increase FF's security and privacy. Should not cause breakage.
+* date: 23 March 2020
+* version 74
+* author: hjs
+* url: https://github.com/hjstephens09/Better-Fox
+* license: MIT: https://github.com/hjstephens09/Better-Fox/blob/master/LICENSE
+
+******/
+
+
+
+/* Third-party cookies
+ * 1=disable third-party cookies, 4=cross site and social media trackers ***/
+	// user_pref("network.cookie.cookieBehavior", 1);
+
+/* Limit third-party cookies */
+user_pref("network.cookie.thirdparty.sessionOnly", true);
+user_pref("network.cookie.thirdparty.nonsecureSessionOnly", true);
+user_pref("pref.privacy.disable_button.cookie_exceptions", false);
+
+
+/** DNS-over-HTTPS ***/
+/* Enter custom DNS Resolver
+ * 0=off, 2=TRR preferred, ?3=TRR only?, 5=TRR disabled ***/
+user_pref("network.trr.mode", 2);
+user_pref("network.trr.uri", "");
+user_pref("network.trr.custom_uri", "");
+user_pref("network.security.esni.enabled", true);
+
+
+/** MISC. ***/
+user_pref("beacon.enabled", false);
+user_pref("browser.cache.offline.enable", false);
+user_pref("browser.fixup.alternate.enabled", false);
+user_pref("browser.fixup.typo.scheme", false);
+user_pref("browser.urlbar.speculativeConnect.enabled", false);
+user_pref("dom.battery.enabled", false);
+user_pref("dom.targetBlankNoOpener.enabled", true);
+user_pref("media.video_stats.enabled", false);
+// user_pref("media.navigator.enabled", false); // breakage?
+user_pref("network.auth.subresource-http-auth-allow", 1);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.ftp.enabled", false);
+user_pref("network.http.referer.defaultPolicy.trackers", 2);
+user_pref("network.http.referer.defaultPolicy", 2);
+user_pref("network.http.speculative-parallel-limit", 0);
+user_pref("network.IDN_show_punycode", true);
+user_pref("network.prefetch-next", false);
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("privacy.donottrackheader.value", 1);
+user_pref("privacy.trackingprotection.enabled", true); // Can turn off w/ uBlock Origin
+user_pref("privacy.trackingprotection.pbmode.enabled", true); // Can turn off w/ uBlock Origin
+user_pref("security.mixed_content.block_active_content", true);
+user_pref("security.mixed_content.block_display_content", true);
+user_pref("security.mixed_content.block_object_subrequest", true);
+user_pref("security.mixed_content.upgrade_display_content", true);
+
+
+/** GEOLOCATION ***/
+user_pref("permissions.default.geo", 0); // 0=default
+/* Use Mozilla geolocation service instead of Google when geolocation is enabled
+ * Optionally enable logging to the console (defaults to false) ***/
+user_pref("geo.provider.network.url", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+/* Disable using the OS's geolocation service ***/
+user_pref("geo.provider.ms-windows-location", false); // [WINDOWS]
+user_pref("geo.provider.use_corelocation", false); // [MAC]
+user_pref("geo.provider.use_gpsd", false); // [LINUX]
+/* Disable GeoIP-based search results
+ * May not be hidden if Firefox has changed your settings due to your locale
+ * [1] https://trac.torproject.org/projects/tor/ticket/16254
+ * [2] https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections#w_geolocation-for-default-search-engine ***/
+user_pref("browser.search.region", "US"); // [HIDDEN PREF]
+user_pref("browser.search.geoip.url", "");
+/* Disable geographically specific results/search engines e.g. "browser.search.*.US"
+ * Ignore all of Mozilla's various search engines in multiple locales ***/
+user_pref("browser.search.geoSpecificDefaults", false);
+user_pref("browser.search.geoSpecificDefaults.url", "");
+
+
+/** SEARCH ***/
+/* Designate a Private Search Engine ***/
+user_pref("browser.search.separatePrivateDefault", true);
+user_pref("browser.search.separatePrivateDefault.ui.enabled", true);
+/* Search Suggestions turned off
+ * Search engines keylog every character you type ***/
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.search.suggest.enabled.private", false);
+	// user_pref("browser.urlbar.suggest.searches", false);
+	// user_pref("browser.urlbar.oneOffSearches", false);
+
+
+/** GOOGLE SAFE BROWSING ***/
+user_pref("browser.safebrowsing.allowOverride", true);
+user_pref("browser.safebrowsing.appRepURL", "");
+user_pref("browser.safebrowsing.blockedURIs.enabled", false);
+user_pref("browser.safebrowsing.downloads.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
+user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+user_pref("browser.safebrowsing.downloads.remote.url", "");
+user_pref("browser.safebrowsing.enabled", false);
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.safebrowsing.phishing.enabled", false);
+
+
+/** MOZILLA ***/
+/* Blocklist ***/
+/* 0401: enforce Firefox blocklist, but sanitize blocklist url
+ * It includes updates for "revoked certificates"
+ * [1] https://blog.mozilla.org/security/2015/03/03/revoking-intermediate-certificates-introducing-onecrl/
+ * [2] https://trac.torproject.org/projects/tor/ticket/16931 ***/
+user_pref("extensions.blocklist.enabled", true); // [DEFAULT: true]
+user_pref("extensions.blocklist.url", "https://blocklists.settings.services.mozilla.com/v1/blocklist/3/%APP_ID%/%APP_VERSION%/");
+
+/* MOZILLA TELEMTRY AND DATA COLLECTION */
+user_pref("app.normandy.api_url", "");
+user_pref("app.normandy.enabled", false);
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("breakpad.reportURL", "");
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit", false);
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+user_pref("browser.crashReports.unsubmittedCheck.enabled", false);
+user_pref("browser.disableResetPrompt", true);
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.ping-centre.telemetry", false);
+user_pref("browser.search.update", false);
+user_pref("browser.selfsupport.url", "");
+user_pref("browser.send_pings.require_same_host", true);
+user_pref("browser.send_pings", false);
+user_pref("browser.startup.homepage_override.mstone", "ignore");
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("datareporting.sessions.current.clean", true);
+user_pref("devtools.onboarding.telemetry.logged", false);
+user_pref("dom.ipc.plugins.flash.subprocess.crashreporter.enabled", false);
+user_pref("dom.ipc.plugins.reportCrashURL", false);
+user_pref("dom.push.enabled", false);
+user_pref("dom.push.userAgentID", "");
+user_pref("experiments.activeExperiment", false);
+user_pref("experiments.enabled", false);
+user_pref("experiments.manifest.uri", "");
+user_pref("experiments.supported", false);
+user_pref("extensions.getAddons.cache.enabled", false);
+user_pref("extensions.getAddons.showPane", false); // [HIDDEN PREF]
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("extensions.webcompat-reporter.enabled", false);
+user_pref("extensions.webservice.discoverURL", "");
+user_pref("network.allow-experiments", false);
+user_pref("security.ssl.errorReporting.automatic", false);
+user_pref("security.ssl.errorReporting.enabled", false);
+user_pref("security.ssl.errorReporting.url", "");
+user_pref("startup.homepage_welcome_url.additional", "");
+user_pref("toolkit.coverage.enabled", false);
+user_pref("toolkit.coverage.endpoint.base", "");
+user_pref("toolkit.coverage.opt-out", true);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.cachedClientID", "");
+user_pref("toolkit.telemetry.coverage.opt-out", true);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.prompted", 2);
+user_pref("toolkit.telemetry.rejected", true);
+user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
+user_pref("toolkit.telemetry.server", "");
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.unifiedIsOptIn", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+
+
+/** OPT ***/
+/* Delete cookies after a certain period of time ***/
+	// user_pref("network.cookie.lifetime.days", 5);
+	// user_pref("network.cookie.lifetimePolicy", 3);
+
+/* Turn off IPv6 when using a VPN ***/
+	// user_pref("network.dns.disableIPv6", true);
+	// user_pref("network.notify.IPv6", false);
+
+/* Nightly only ***/
+	//user_pref("fission.autostart", true);
+	//user_pref("privacy.purge_trackers.enabled", true);
+	//user_pref("privacy.purge_trackers.logging.enabled", false);
+	//user_pref("privacy.purge_trackers.max_purge_count", 100); // default=100
+	//user_pref("dom.security.https_only_mode", true); // may cause breakage
+
