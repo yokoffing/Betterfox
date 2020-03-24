20f87a40baef234f417b38103e6ba0e4fdf4a82f
hjstephens09 <hjstephens09@gmail.com>
2020-03-24 10:52:27 -0400

    Update PeskyFox.js

--- PeskyFox.js
+++ PeskyFox.js
@@ -2,7 +2,7 @@
 
 * name: PeskyFox
 * description: Take away annoyances and adds a few options
-* date: 23 March 2020
+* date: 24 March 2020
 * version 74
 * authors: hjs
 * url: https://github.com/hjstephens09/Better-Fox
@@ -14,22 +14,21 @@
 /* Download location
  * 0=desktop, 1=downloads (default), 2=last used
  * [SETTING] To set your default "downloads": General>Downloads>Save files to ***/
-user_pref("browser.download.folderList", 1);
+	// user_pref("browser.download.folderList", 1);
 
 /* Enforce user interaction for security by always asking where to download
  * On Android this blocks longtapping and saving images
  * [SETTING] General>Downloads>Always ask you where to save files ***/
-	//user_pref("browser.download.useDownloadDir", false);
+	// user_pref("browser.download.useDownloadDir", false);
 
 /* Disable adding downloads to the system's "recent documents" list ***/
-user_pref("browser.download.manager.addToRecentDocs", false);
+user_pref("browser.download.manager.addToRecentDocs", false); // for privacy or annoyance
 
 /* Disable hiding mime types (Options>General>Applications) not associated with a plugin ***/
 user_pref("browser.download.hide_plugins_without_extensions", false);
 
 /* Autohide download button ***/
 user_pref("browser.download.autohideButton", true);
-
 
 
 /** WARNINGS ***/
@@ -49,7 +48,10 @@ user_pref("browser.in-content.dark-mode", true);
 user_pref("ui.systemUsesDarkTheme", 1); // [HIDDEN PREF]
 
 
-/** MISC ***/
+/** OTHER CONFIGS ***/
+
+/* Set START page (0=blank, 1=home, 2=last visited page, 3=resume previous session) ***/
+user_pref("browser.startup.page", 3);
 
 /* Disable backspace action ***/
 user_pref("browser.backspace_action", 2); // 0=previous page, 1=scroll up, 2=do nothing
@@ -70,37 +72,29 @@ user_pref("permissions.default.desktop-notification", 2);
 /* Insert tab immediately after the current tab
  * Tap to Tab extension: set to "Put new tab at the end"
  * extension: https://addons.mozilla.org/en-US/firefox/addon/tap-to-tab ***/
-user_pref("browser.tabs.insertAfterCurrent", true);
-user_pref("browser.tabs.insertRelatedAfterCurrent", false);
-
-/* Prevent FF from going offline
- * FF manages network connectivity poorly. Best to disable unless you need it.
- * [1] https://lifehacker.com/stop-firefox-from-automatically-entering-work-offline-5714560 ***/
-user_pref("network.manage-offline-status", false);
+	// user_pref("browser.tabs.insertAfterCurrent", true);
+	// user_pref("browser.tabs.insertRelatedAfterCurrent", false);
+	// user_pref("browser.ctrlTab.recentlyUsedOrder", false); // chronological order
 
 
-
-/* Other **/
+/** MISC. **/
 user_pref("browser.tabs.loadBookmarksInBackground", true);
 user_pref("browser.tabs.loadBookmarksInTabs", true);
-user_pref("browser.tabs.tabMinWidth", 100);
+user_pref("browser.tabs.tabMinWidth", 100); // default=76, adjust to preference
 user_pref("devtools.debugger.ui.editor-wrapping", true);
 user_pref("media.autoplay.default", 1); // 0=Allow all, 1=Block non-muted media (default), 5=Block all
-user_pref("toolkit.zoomManager.zoomValues", ".8,.9,.95,1,1.05,1.1,1,15,1.2,1.33,1.5,1.7,2,2.4");
+user_pref("toolkit.zoomManager.zoomValues", ".8,.9,.95,1,1.05,1.1,1,15,1.2,1.33,1.5,1.7,2,2.4"); // more controlled zooming experience
 user_pref("view_source.wrap_long_lines", true);
-user_pref("config.trim_on_minimize", true);
+user_pref("config.trim_on_minimize", true); // [WINDOWS-ONLY] saves memory when FF is minimized
 user_pref("layout.spellcheckDefault", 2);
-user_pref("browser.urlbar.decodeURLsOnCopy", true); // see bugzilla 1320061 [FF53+]
+user_pref("browser.urlbar.decodeURLsOnCopy", true); // see bugzilla 1320061
 /* [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1320061 ***/
 user_pref("general.autoScroll", false);
-user_pref("middlemouse.scrollbarPosition", true);
+// user_pref("middlemouse.scrollbarPosition", true);
 user_pref("ui.key.menuAccessKey", 0);
 user_pref("accessibility.force_disabled", 1);
 user_pref("browser.bookmarks.max_backups", 2);
-user_pref("browser.ctrlTab.recentlyUsedOrder", false);
-user_pref("browser.download.folderList", 0);
 user_pref("browser.shell.checkDefaultBrowser", false);
-user_pref("browser.startup.page", 3);
 user_pref("browser.urlbar.trimURLs", true);
 user_pref("extensions.getAddons.cache.enabled", false);
 user_pref("extensions.getAddons.showPane", false);
@@ -142,12 +136,5 @@ user_pref("browser.toolbarbuttons.introduced.pocket-button", false);
 
 
 /* OPTIONAL ***/
-
-/* Hide image placeholders ***/
-//user_pref("browser.display.show_image_placeholders", false);
-
-/* Disable Firefox animations ***/
-//user_pref("toolkit.cosmeticAnimations.enabled", false);
-
-//user_pref("dom.event.contextmenu.enabled", false);
-//user_pref("browser.tabs.closeWindowWithLastTab", false);
+	// user_pref("dom.event.contextmenu.enabled", false);
+	// user_pref("browser.tabs.closeWindowWithLastTab", false);
