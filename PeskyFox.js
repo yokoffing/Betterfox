6971fefdda47958c7413e504e91a60e9b8161ff1
yokoffing <hjstephens09@gmail.com>
2020-07-30 10:50:15 -0400

    clarified new tab page items

--- PeskyFox.js
+++ PeskyFox.js
@@ -11,32 +11,9 @@
  * PeskyFox                                                                 *
  * "Aquila non capit muscas."                                               *
  * priority: remove annoyances                                              *
- * version: 5 June 2020                                                     *
+ * version: 30 July 2020                                                    *
  * url: https://github.com/yokoffing/Better-Fox                             *
  ***************************************************************************/
-
-/****************************************************************************
- * SECTION: DARK MODE                                                       *
-****************************************************************************/
-
-// PREF: Dark pages in settings pages
-// user_pref("browser.in-content.dark-mode", true);
-
-// PREF: Enforce prefers-color-scheme as Dark
-// 0=light, 1=dark : This overrides your OS value.
-// user_pref("ui.systemUsesDarkTheme", 1); // [HIDDEN PREF]
-
-// PREF: Disable webextension restrictions on Mozilla domains
-// If you use an extension to turn your webpages to Dark Mode, this prevents
-// you from being blinded by white flash when navigating to a Mozilla domain.
-// ! RESTART
-// https://bugzilla.mozilla.org/buglist.cgi?bug_id=1384330,1406795,1415644,1453988
-// user_pref("extensions.webextensions.restrictedDomains", "");
-// PREF: disable mozAddonManager Web API [FF57+]
- * [NOTE] As a side-effect in FF57-59 this allowed extensions to work on AMO. In FF60+ you also need
- * to sanitize or clear extensions.webextensions.restrictedDomains (see 2662) to keep that side-effect
- * [1] https://bugzilla.mozilla.org/buglist.cgi?bug_id=1384330,1406795,1415644,1453988 ***/
-user_pref("privacy.resistFingerprinting.block_mozAddonManager", true); // [HIDDEN PREF]
 
 /****************************************************************************
  * SECTION: START-UP / NEW TAB PAGE                                         *
@@ -46,35 +23,101 @@ user_pref("privacy.resistFingerprinting.block_mozAddonManager", true); // [HIDDE
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 // PREF: Set START page (0=blank, 1=home, 2=last visited page, 3=resume previous session)
-user_pref("browser.startup.page", 3);
+// user_pref("browser.startup.page", 3);
 
-// PREF: Unclutter the new tab page
-user_pref("browser.library.activity-stream.enabled", false);
+// PREF: Disable Extension Recommendations (CFR: "Contextual Feature Recommender")
+// https://support.mozilla.org/en-US/kb/extension-recommendations
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
-user_pref("browser.newtabpage.activity-stream.discoverystream.enabled", false);
-user_pref("browser.newtabpage.activity-stream.feeds.discoverystreamfeed", false);
-user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
-user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
-user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
-user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
-user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+
+// PREF: Disable Activity Stream Top Stories, Pocket-based and/or sponsored content
 user_pref("browser.newtabpage.activity-stream.pocketCta", "");
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories.options", "");
+user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+user_pref("browser.newtabpage.activity-stream.sectionOrder", "");
 user_pref("browser.newtabpage.activity-stream.showSponsored", false);
-user_pref("browser.newtabpage.enhanced", false);
-user_pref("browser.newtabpage.introShown", false);
+user_pref("browser.newtabpage.activity-stream.discoverystream.enabled", false);
+
+// PREF: Disable Activity Stream snippets
+// Runs code received from a server (aka Remote Code Execution) and sends information back to a metrics server
 user_pref("browser.newtabpage.activity-stream.asrouter.providers.snippets", "");
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+
+// PREF: Disable Activity Stream telemetry
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry.ut.events", false);
+user_pref("browser.newtabpage.activity-stream.telemetry.structuredIngestion", false);
+user_pref("browser.newtabpage.activity-stream.telemetry.structuredIngestion.endpoint", "");
+
+// PREF: Disable Activity Stream feeds
+user_pref("browser.newtabpage.activity-stream.feeds.aboutpreferences", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.favicon", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.messagecenterfeed", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.migration", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.newtabinit", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.places", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.prefs", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.sections", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.systemtick", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.asrouterfeed", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.recommendationproviderswitcher", false);  
+
+// PREF: Disable Activity Stream (others)
+user_pref("browser.newtabpage.activity-stream.messageCenterExperimentEnabled", false);
+user_pref("browser.newtabpage.activity-stream.showSearch", false);  
+user_pref("browser.newtabpage.activity-stream.discoverystream.config", "");  
+user_pref("browser.newtabpage.activity-stream.discoverystream.endpoints", "");  
+user_pref("browser.newtabpage.activity-stream.discoverystream.rec.impressions", "");  
+user_pref("browser.newtabpage.activity-stream.discoverystream.spoc.impressions", "");  
+user_pref("browser.newtabpage.activity-stream.discoverystream.endpointSpocsClear", "");
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeBookmarks", false);  
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeDownloads", false);  
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", false);  
+user_pref("browser.newtabpage.activity-stream.discoverystream.personalization.modelKeys", "");  
+user_pref("browser.newtabpage.activity-stream.improvesearch.handoffToAwesomebar", false);  
+user_pref("browser.newtabpage.activity-stream.discoverystream.isCollectionDismissible", false);  
+user_pref("browser.newtabpage.activity-stream.discoverystream.region-basic-layout", false);  
+user_pref("browser.newtabpage.activity-stream.discoverystream.region-layout-config", "");  
+user_pref("browser.newtabpage.activity-stream.discoverystream.region-spocs-config", "");  
+user_pref("browser.newtabpage.activity-stream.discoverystream.region-stories-config", "");  
+user_pref("browser.newtabpage.activity-stream.discoverystream.recs.personalized", false);  
+user_pref("browser.newtabpage.activity-stream.discoverystream.spocs.personalized", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.system.topsites", false);  
+user_pref("browser.newtabpage.activity-stream.feeds.system.topstories", false); 
+
+// PREF: Disable new tab tile ads and preload
+// https://wiki.mozilla.org/Tiles/Technical_Documentation#Ping
+// https://gecko.readthedocs.org/en/latest/browser/browser/DirectoryLinksProvider.html#browser-newtabpage-directory-source
+// https://gecko.readthedocs.org/en/latest/browser/browser/DirectoryLinksProvider.html#browser-newtabpage-directory-ping
+user_pref("browser.newtab.preload", false);  
+user_pref("browser.newtabpage.directory.ping", "");  
+user_pref("browser.newtabpage.activity-stream.asrouter.messageProviders", ""); 
+
+// PREF: Don't reveal build ID
+// Value taken from Tor Browser
+// https://bugzilla.mozilla.org/show_bug.cgi?id=583181
+// user_pref("browser.startup.homepage_override.mstone", "ignore");
+
+// PREF: Disable separate about:welcome page
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1617783
+// user_pref("browser.aboutwelcome.enabled", false);
+// user_pref("trailhead.firstrun.branches", "");
+// user_pref("browser.aboutwelcome.overrideContent", "");
 
 /******************************************************************************
  * SECTION: DISABLE POCKET EXTENSION                                          *
 ******************************************************************************/
 
-// PREF: Completely remove Pocket from Firefox
+// PREF: Disable built-in Pocket extension
+user_pref("browser.pocket.enabled", false);
 user_pref("extensions.pocket.api"," ");
 user_pref("extensions.pocket.enabled", false);
 user_pref("extensions.pocket.oAuthConsumerKey", " ");
 user_pref("extensions.pocket.site", " ");
-user_pref("browser.toolbarbuttons.introduced.pocket-button", false);
 
 /******************************************************************************
  * SECTION: DOWNLOADS                                 *
@@ -116,16 +159,19 @@ user_pref("browser.aboutConfig.showWarning", false);
  * SECTION: AUTOFILL                                                        *
 ****************************************************************************/
 
-// PREF: Disable Autofill
-// Not recommended to use the browser's innate autofill.
-// You can use a password manager like Bitwarden for an encrypted,
-// auto-fill alternative.
+// PREF: Disable Form Autofill
+// NOTE: stored data is not secure (uses a JSON file)
+// https://wiki.mozilla.org/Firefox/Features/Form_Autofill
+// https://www.ghacks.net/2017/05/24/firefoxs-new-form-autofill-is-awesome
 user_pref("extensions.formautofill.addresses.enabled", false);
 user_pref("extensions.formautofill.available", "off");
 user_pref("extensions.formautofill.creditCards.enabled", false);
 user_pref("extensions.formautofill.heuristics.enabled", false);
-user_pref("signon.autofillForms", false);
 user_pref("browser.formfill.enable", false);
+
+// PREF: Disable autofilling saved passwords on HTTP pages and show warning
+user_pref("signon.autofillForms", false);
+user_pref("security.insecure_field_warning.contextual.enabled", true);
 
 // PREF: Disable saving passwords
 user_pref("signon.rememberSignons", false);
@@ -139,7 +185,7 @@ user_pref("signon.formlessCapture.enabled", false);
 user_pref("browser.shell.checkDefaultBrowser", false);
 
 // PREF: Hide parts of the url in the location bar
-user_pref("browser.urlbar.trimURLs", true);
+// user_pref("browser.urlbar.trimURLs", true);
 
 // PREF: Disable annoying website notifications
 user_pref("permissions.default.desktop-notification", 2);
@@ -158,7 +204,7 @@ user_pref("media.autoplay.default", 5);
 user_pref("browser.backspace_action", 2);
 
 // PREF: Disable ALT key toggling the menu bar
-// user_pref("ui.key.menuAccessKey", 0);
+user_pref("ui.key.menuAccessKey", 0);
 
 // PREF: Disable website control over browser right-click context menu
 // [!] This sometimes results in having two menus displayed.
@@ -185,11 +231,11 @@ user_pref("layout.spellcheckDefault", 2);
 user_pref("accessibility.force_disabled", 1);
 
 // PREF: Limit the number of bookmark backups Firefox keeps
-user_pref("browser.bookmarks.max_backups", 2);
+// user_pref("browser.bookmarks.max_backups", 2);
 
 // PREF: Allow for more granular control of zoom levels
 // Especially useful if you want to set your default zoom to a custom level
-// user_pref("toolkit.zoomManager.zoomValues", ".3,.5,.67,.8,.9,.95,1,1.05,1.1,1.15,1.2,1.25,1.33,1.5,1.7,2,2.4,3");
+// user_pref("toolkit.zoomManager.zoomValues", ".3,.5,.67,.8,.9,.95,1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,2,2.4,3");
 
 // PREF: Hide image placeholders
 user_pref("browser.display.show_image_placeholders", false);
@@ -220,17 +266,17 @@ user_pref("browser.link.open_newwindow", 3);
  
 // PREF: This preference overrides <browser.link.open_newwindow> for external
 // links. Set if a different destination for external links is needed.
-// -1=no overrides (default)
 // 1=Open in the current tab/window
 // 2=Open in a new window
 // 3=Open in a new tab in the current window
+// -1=no overrides (default)
 // user_pref("browser.link.open_newwindow.override.external", -1);
 
 // PREF: Open links targeting new windows in a new tab instead
 // Pop-up windows are treated like regular tabs
 // You can still right-click a link and open in a new window
 user_pref("browser.link.open_newwindow", 3);
-user_pref("browser.link.open_newwindow.restriction", 0);
+// user_pref("browser.link.open_newwindow.restriction", 0);
 
 // PREF: Insert tab immediately after the current tab
 // Tap to Tab extension: set to "Put new tab at the end"
@@ -248,19 +294,15 @@ user_pref("browser.bookmarks.openInTabClosesMenu", false);
 user_pref("browser.tabs.loadBookmarksInBackground", true);
 user_pref("browser.tabs.loadBookmarksInTabs", true);
 
-// PREF: Show Picture-in-Picture (PiP) flyout
-user_pref("media.videocontrols.picture-in-picture.audio-toggle.enabled", true);
-user_pref("media.videocontrols.picture-in-picture.video-toggle.flyout-enabled", true);
-
 // PREF: Stop websites from reloading pages automatically
 // https://www.ghacks.net/2018/08/19/stop-websites-from-reloading-pages-automatically/
-user_pref("accessibility.blockautorefresh", true);
-user_pref("browser.meta_refresh_when_inactive.disabled", true);
+// user_pref("accessibility.blockautorefresh", true);
+// user_pref("browser.meta_refresh_when_inactive.disabled", true);
 
 // PREF: Color management
 // Force FF to show the same color profiles as Chromium
-user_pref("gfx.color_management.mode", 1);
-user_pref("gfx.color_management.enablev4", true);
+// user_pref("gfx.color_management.mode", 1);
+// user_pref("gfx.color_management.enablev4", true);
 
 // [FF 77+] PREF: Firefox now allows viewing of PDFs even if the response HTTP headers
 // include Content-Disposition:attachment. 
