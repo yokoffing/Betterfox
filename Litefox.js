b922224a1034c32783e484cf7fce68b79921cb7f
yokoffing <hjstephens09@gmail.com>
2020-07-30 10:48:07 -0400

    added pocket and telemetry removal

--- Litefox.js
+++ Litefox.js
@@ -3,7 +3,7 @@
  * Litefox                                                                  *
  * "Facile est inventis addere."                                            *
  * priority: the simple, bare necessities                                   *
- * version: 29 July 2020                                                    *
+ * version: 30 July 2020                                                    *
  * url: https://github.com/hjstephens09/Better-Fox                          *
  ***************************************************************************/
 
@@ -24,8 +24,20 @@ user_pref("privacy.socialtracking.block_cookies.enabled", true);
 user_pref("privacy.trackingprotection.cryptomining.enabled", true);
 user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
 user_pref("network.cookie.cookieBehavior", 1);
+user_pref("pref.privacy.disable_button.cookie_exceptions", false);
+user_pref("network.cookie.thirdparty.sessionOnly", true);
+user_pref("network.cookie.thirdparty.nonsecureSessionOnly", true);
 user_pref("browser.search.separatePrivateDefault", true);
 user_pref("browser.search.separatePrivateDefault.ui.enabled", true);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.search.suggest.enabled.private", false);
+user_pref("browser.fixup.alternate.enabled", false);
+user_pref("browser.fixup.typo.scheme", false);
+user_pref("security.mixed_content.block_active_content", true);
+user_pref("dom.battery.enabled", false);
+user_pref("dom.targetBlankNoOpener.enabled", true);
+user_pref("network.IDN_show_punycode", true);
+user_pref("browser.urlbar.decodeURLsOnCopy", true);
 user_pref("browser.safebrowsing.allowOverride", true);
 user_pref("browser.safebrowsing.appRepURL", "");
 user_pref("browser.safebrowsing.blockedURIs.enabled", false);
@@ -38,7 +50,7 @@ user_pref("browser.safebrowsing.enabled", false);
 user_pref("browser.safebrowsing.malware.enabled", false);
 user_pref("browser.safebrowsing.phishing.enabled", false);
 user_pref("geo.provider.network.url", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
-user_pref("dom.battery.enabled", false);
+user_pref("geo.provider.network.logging.enabled", false);
 user_pref("toolkit.telemetry.unified", false);
 user_pref("toolkit.telemetry.enabled", false);
 user_pref("toolkit.telemetry.server", "data:,");
@@ -65,14 +77,27 @@ user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
 ****************************************************************************/
 user_pref("browser.download.manager.addToRecentDocs", false);
 user_pref("browser.startup.page", 3);
+user_pref("browser.newtabpage.activity-stream.pocketCta", "");
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories.options", "");
+user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+user_pref("browser.newtabpage.activity-stream.sectionOrder", "");
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.discoverystream.enabled", false);
+user_pref("browser.pocket.enabled", false);
+user_pref("extensions.pocket.api"," ");
 user_pref("extensions.pocket.enabled", false);
-user_pref("toolkit.zoomManager.zoomValues", ".3,.5,.67,.8,.9,1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,2,2.4,3");
+user_pref("extensions.pocket.oAuthConsumerKey", " ");
+user_pref("extensions.pocket.site", " ");
 user_pref("full-screen-api.warning.delay", 0);
 user_pref("full-screen-api.warning.timeout", 0);
-user_pref("dom.forms.autocomplete.formautofill", false);
 user_pref("extensions.formautofill.addresses.enabled", false);
 user_pref("extensions.formautofill.available", "off");
 user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("extensions.formautofill.heuristics.enabled", false);
+user_pref("browser.formfill.enable", false);
+user_pref("signon.autofillForms", false);
+user_pref("security.insecure_field_warning.contextual.enabled", true);
 user_pref("signon.rememberSignons", false);
 user_pref("signon.formlessCapture.enabled", false);
 user_pref("browser.shell.checkDefaultBrowser", false);
@@ -82,13 +107,8 @@ user_pref("layout.spellcheckDefault", 2);
 user_pref("findbar.highlightAll", true);
 user_pref("view_source.wrap_long_lines", true);
 user_pref("devtools.debugger.ui.editor-wrapping", true);
-user_pref("devtools.theme", "dark");
 user_pref("browser.link.open_newwindow", 3);
-user_pref("browser.link.open_newwindow.restriction", 3);
-user_pref("browser.link.open_newwindow.override.external", -1);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
-user_pref("browser.tabs.loadBookmarksInBackground", true);
-user_pref("browser.tabs.loadBookmarksInTabs", true);
 user_pref("editor.truncate_user_pastes", false);
 
 /****************************************************************************
