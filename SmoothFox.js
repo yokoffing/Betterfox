d467b68e5e559e9c15b72a9af03e1b7efa865e7c
yokoffing <hjstephens09@gmail.com>
2020-09-24 12:49:56 -0400

    Update SmoothFox.js

--- SmoothFox.js
+++ SmoothFox.js
@@ -11,15 +11,34 @@
  * SmoothFox                                                                *
  * "Faber est suae quisque fortunae."                                       *
  * priority: smooth scrolling                                               *
- * version: 3 April 2020                                                    *
+ * version: 24 September 2020                                                    *
  * url: https://github.com/hjstephens09/Better-Fox                          *
  ***************************************************************************/
+
+/****************************************************************************
+ * SECTION: SMOOTH SCROLLING (OVERALL IMPROVEMENT OF DEFAULT                *
+****************************************************************************/
+user_pref("general.smoothScroll", true);
+user_pref("general.smoothScroll.lines.durationMaxMS", 125);
+user_pref("general.smoothScroll.lines.durationMinMS", 125);
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 200);
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 100);
+user_pref("general.smoothScroll.other.durationMaxMS", 125);
+user_pref("general.smoothScroll.other.durationMinMS", 125);
+user_pref("general.smoothScroll.pages.durationMaxMS", 125);
+user_pref("general.smoothScroll.pages.durationMinMS", 125);
+user_pref("mousewheel.system_scroll_override_on_root_content.horizontal.factor", 175);
+user_pref("mousewheel.system_scroll_override_on_root_content.vertical.factor", 175);
+user_pref("toolkit.scrollbox.horizontalScrollDistance", 6);
+user_pref("toolkit.scrollbox.verticalScrollDistance", 2);
+user_pref("mousewheel.min_line_scroll_amount", 40);
 
 /****************************************************************************
  * SECTION: EDGE-LIKE SMOOTH SCROLLING (USING MSDPHYSICS)                   *
 ****************************************************************************/
 // PREF: Edge-like smooth scrolliing (using msdPhysics)
 user_pref("apz.frame_delay.enabled", false);
+user_pref("general.smoothScroll", true);
 user_pref("general.smoothScroll.msdPhysics.enabled", true);
 user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 250);
 user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 400);
@@ -36,6 +55,7 @@ user_pref("mousewheel.min_line_scroll_amount", 35); // adjust to preference, 20-
 ******************************************************************************/
 // PREF: Edge-like smooth scrolliing (without msdPhysics)
 user_pref("apz.frame_delay.enabled", false);
+user_pref("general.smoothScroll", true);
 user_pref("general.smoothScroll.currentVelocityWeighting", 0.05);
 user_pref("general.smoothScroll.lines.durationMaxMS", 100);
 user_pref("general.smoothScroll.lines.durationMinMS", 450);
@@ -57,6 +77,7 @@ user_pref("mousewheel.min_line_scroll_amount", 35); // adjust to preference, 20-
 // PREF: OS X / macOS Trackpad
 // May not need to adjust settings since scrolling is already pretty smooth in macOS.
 user_pref("apz.frame_delay.enabled", false);
+user_pref("general.smoothScroll", true);
 user_pref("general.smoothScroll.mouseWheel", true);
 user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 600);
 user_pref("general.smoothScroll.mouseWheel.durationMinMS", 400);
@@ -66,14 +87,3 @@ user_pref("general.smoothScroll.stopDecelerationWeighting", 0.8);
 user_pref("mousewheel.acceleration.factor", 8);
 user_pref("mousewheel.acceleration.start", 2);
 user_pref("mousewheel.min_line_scroll_amount", 2);
-
-/******************************************************************************
- * SECTION: GPU SPECIFIC                                                      *
-******************************************************************************/
-
-// PREF: Improve performance by utilizing the GPU
-// https://cialu.net/how-to-turn-on-accelerated-compositing-layers-on-firefox/
-user_pref("layers.acceleration.force-enabled", true);
-
-// PREF: GPU + Linux-only
-user_pref("layers.accelerate-all", true);
