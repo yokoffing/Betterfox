49dd2cf43363ae3b04a3257e831d5613911038e6
yokoffing <hjstephens09@gmail.com>
2021-01-29 16:28:06 -0500

    29 Jan 2021
    Added/Changed:
    user_pref("privacy.trackingprotection.enabled", true);
    user_pref("urlclassifier.trackingSkipURLs", "*.twitter.com, *.twimg.com");
    user_pref("privacy.trackingprotection.socialtracking.enabled", true);
    user_pref("urlclassifier.features.socialtracking.skipURLs", "*.instagram.com, *.twitter.com, *.twimg.com");
    user_pref("dom.security.https_only_mode.upgrade_local", true);
    user_pref("network.auth.subresource-http-auth-allow", 1);
    user_pref("dom.block_download_insecure", true);
    user_pref("network.http.referer.XOriginPolicy", 0);
    user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
    user_pref("extensions.postDownloadThirdPartyPrompt", false);
    user_pref("pdfjs.defaultZoomValue", "page-width");
    user_pref("extensions.pocket.api"," ");
    user_pref("extensions.pocket.oAuthConsumerKey", " ");
    user_pref("extensions.pocket.site", " ");
    
    * PDF prefs now have their own section in Peskyfox. Mozilla has been adding some prefs for their built-in PDF reader every few months.

--- user.js
+++ user.js
@@ -1,49 +1,91 @@
 //
+/* You may COPY+PASTE this file.
+ *
+ * HOWEVER, please confirm the following preferences:
+ *
+ * [1] Google Safe Browsing (disabled)
+ * [2] Firefox's Password Manager (disabled)
+ * [3] Firefox's Address and Credit Card management (disabled)
+ * [4] DNS-over-HTTPS (recommended)
+ *
+ * [1] If you do NOT have a firewall or DNS-filtering for dangerous and
+ * deceptive content, then delete all prefs related to Google Safe Browsing.
+ * Trading security for a little bit of privacy is worth it.
+ *
+ * [2] [3] If you use these native features of Firefox, please remove the
+ * prefs as instructed below.
+ *
+ * If you make changes to your about:config while the program is running, the
+ * changes will be overwritten when the application restarts.
+ *
+ * To make lasting changes to preferences, you will have to edit the user.js.
+ */
+
 /****************************************************************************
  * BetterFox                                                                *
- * name: yokoffing user.js                                                  *
- * version: 16 January 2021                                                 *
+ * "Ad meliora."                                                            *
+ * version: 29 January 2021                                                 *
  * url: https://github.com/yokoffing/Better-Fox                             *
  * license: https://github.com/yokoffing/Better-Fox/blob/master/LICENSE     *
  * README: https://github.com/yokoffing/Better-Fox/blob/master/README.md    *
- * "Ad meliora."                                                            *
 ****************************************************************************/
 
+/****************************************************************************
+ * START: BETTERFOX                                                         *
+****************************************************************************/
 
 /****************************************************************************
- * START: FASTFOX                                                           *
+ * SECTION: FASTFOX                                                         *
 ****************************************************************************/
 user_pref("gfx.webrender.all", true);
-user_pref("full-screen-api.transition-duration.enter", "0 0");
-user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("javascript.options.warp", true);
 user_pref("dom.image-lazy-loading.enabled", true);
-
+user_pref("browser.sessionstore.restore_tabs_lazily", true);
+user_pref("browser.sessionstore.restore_on_demand", true);
+user_pref("browser.sessionstore.restore_pinned_tabs_on_demand", true);
+user_pref("browser.startup.preXulSkeletonUI", false);
 
 /****************************************************************************
- * START: SECUREFOX                                                         *
+ * SECTION: SECUREFOX                                                       *
 ****************************************************************************/
+
+/** TRACKING PROTECTION ***/
+user_pref("privacy.trackingprotection.enabled", true);
 user_pref("privacy.trackingprotection.pbmode.enabled", true);
-user_pref("privacy.socialtracking.block_cookies.enabled", true);
 user_pref("privacy.trackingprotection.cryptomining.enabled", true);
 user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
-user_pref("privacy.trackingprotection.socialtracking.enabled", false);
+user_pref("privacy.trackingprotection.socialtracking.enabled", true);
+user_pref("urlclassifier.trackingSkipURLs", "*.twitter.com, *.twimg.com");
+user_pref("urlclassifier.features.socialtracking.skipURLs", "*.instagram.com, *.twitter.com, *.twimg.com");
 user_pref("browser.send_pings", false);
 user_pref("browser.send_pings.require_same_host", true);
 user_pref("beacon.enabled", false);
 user_pref("dom.battery.enabled", false);
 
-/*** STORAGE ***/
+/** STORAGE ***/
 user_pref("network.cookie.cookieBehavior", 5);
 user_pref("privacy.purge_trackers.enabled", true);
-user_pref("browser.cache.offline.enable", false);
 user_pref("browser.cache.cache_isolation", true);
+user_pref("browser.cache.disk.enable", true);
+user_pref("browser.cache.offline.enable", true);
+user_pref("browser.cache.offline.storage.enable", false);
 user_pref("privacy.partition.network_state", true);
 user_pref("dom.storage.next_gen", true);
+
+/** CLEARING HISTORY DEFAULTS ***/
+user_pref("privacy.cpd.history", true);
+user_pref("privacy.cpd.formdata", true);
+user_pref("privacy.cpd.offlineApps", true);
+user_pref("privacy.cpd.cache", true);
+user_pref("privacy.cpd.cookies", false);
+user_pref("privacy.cpd.sessions", false);
+user_pref("privacy.cpd.siteSettings", false);
+user_pref("privacy.sanitize.timeSpan", 0);
 
 /*** PRELOADING ***/
 user_pref("network.dns.disablePrefetch", true);
 user_pref("network.dns.disablePrefetchFromHTTPS", true);
-user_pref("browser.urlbar.speculativeConnect.enabled", true);
+user_pref("browser.urlbar.speculativeConnect.enabled", false);
 user_pref("network.prefetch-next", false);
 user_pref("network.http.speculative-parallel-limit", 6);
 user_pref("network.preload", true);
@@ -52,24 +94,35 @@ user_pref("network.predictor.enable-hover-on-ssl", true);
 user_pref("network.predictor.enable-prefetch", false);
 user_pref("browser.newtab.preload", true);
 
-/*** SEARCH ***/
+/** SEARCH ***/
 user_pref("browser.search.separatePrivateDefault", true);
 user_pref("browser.search.separatePrivateDefault.ui.enabled", true);
 user_pref("browser.search.suggest.enabled", false);
 user_pref("browser.search.suggest.enabled.private", false);
 user_pref("browser.fixup.alternate.enabled", false);
 user_pref("security.insecure_connection_text.enabled", true);
+user_pref("security.insecure_connection_text.pbmode.enabled", true);
 user_pref("network.IDN_show_punycode", true);
 
-/*** HTTPS-ONLY MODE ***/
+/** HTTPS-ONLY MODE ***/
 user_pref("dom.security.https_only_mode", true);
 user_pref("dom.security.https_only_mode_ever_enabled", true);
 user_pref("dom.security.https_only_mode_send_http_background_request", false);
+user_pref("dom.security.https_only_mode.upgrade_local", true);
 
-/***PASSWORDS AND AUTOFILL***/
-user_pref("signon.management.page.breach-alerts.enabled", true);
+/** DNS-over-HTTPS (DOH) ***/
+/* NOTE: Uncommented these prefs and set your DoH URI.
+* Or add your custom prefs at the end of this document to override.
+* user_pref("network.trr.mode", 3);
+* user_pref("network.trr.uri", "");
+* user_pref("network.trr.custom_uri", ""); ***/
+user_pref("network.trr.send_user-agent_headers", false);
+user_pref("network.dns.skipTRR-when-parental-control-enabled", false);
+
+/** PASSWORDS AND AUTOFILL ***/
 user_pref("signon.autofillForms.http", false);
 user_pref("security.insecure_field_warning.contextual.enabled", true);
+/* NOTE: Remove everything below this line if you use Firefox's password manager */
 user_pref("signon.rememberSignons", false);
 user_pref("signon.rememberSignons.visibilityToggle", false);
 user_pref("signon.schemeUpgrades", false);
@@ -77,6 +130,9 @@ user_pref("signon.showAutoCompleteFooter", false);
 user_pref("signon.autologin.proxy", false);
 user_pref("signon.privateBrowsingCapture.enabled", false);
 user_pref("signon.debug", false);
+user_pref("signon.generation.available", false);
+user_pref("signon.generation.enabled", false);
+user_pref("signon.management.page.fileImport.enabled", false);
 user_pref("signon.importedFromSqlite", false);
 user_pref("signon.recipes.path", "");
 user_pref("signon.autofillForms", false);
@@ -84,23 +140,35 @@ user_pref("signon.autofillForms.autocompleteOff", true);
 user_pref("signon.showAutoCompleteOrigins", false);
 user_pref("signon.storeWhenAutocompleteOff", false);
 user_pref("signon.formlessCapture.enabled", false);
-user_pref("signon.generation.available", false);
-user_pref("signon.generation.enabled", false);
 
-/*** MIXED CONTENT ***/
+/** ADDRESS + CREDIT CARD MANAGER ***/
+/* NOTE: Remove everything below this line if you use this feature */
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.available", "off");
+user_pref("extensions.formautofill.creditCards.available", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("extensions.formautofill.heuristics.enabled", false);
+user_pref("browser.formfill.enable", false);
+
+/** MIXED CONTENT ***/
+user_pref("network.auth.subresource-http-auth-allow", 1);
 user_pref("security.mixed_content.block_active_content", true);
 user_pref("security.mixed_content.upgrade_display_content", true);
 user_pref("security.mixed_content.block_object_subrequest", true);
+user_pref("dom.block_download_insecure", true);
+user_pref("extensions.postDownloadThirdPartyPrompt", false);
+user_pref("permissions.delegation.enabled", false);
 
 /*** VARIOUS ***/
 user_pref("dom.targetBlankNoOpener.enabled", true);
-user_pref("pdfjs.disabled", false);
 user_pref("privacy.window.name.update.enabled", true);
+user_pref("network.http.referer.XOriginPolicy", 0);
+user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
 user_pref("security.pki.crlite_mode", 2);
 user_pref("security.remote_settings.crlite_filters.enabled", true);
 
-/***GOOGLE SAFE BROWSING***/
-/* Be sure to have alternate security measures if you disable Safebrowsing! */
+/** GOOGLE SAFE BROWSING ***/
+/* WARNING: Be sure to have alternate security measures if you disable Safebrowsing! */
 user_pref("browser.safebrowsing.allowOverride", true);
 user_pref("browser.safebrowsing.appRepURL", "");
 user_pref("browser.safebrowsing.blockedURIs.enabled", false);
@@ -113,18 +181,22 @@ user_pref("browser.safebrowsing.enabled", false);
 user_pref("browser.safebrowsing.malware.enabled", false);
 user_pref("browser.safebrowsing.phishing.enabled", false);
 
-/***MOZILLA GEOLOCATION INSTEAD OF GOOGLE***/
+/** MOZILLA GEOLOCATION INSTEAD OF GOOGLE ***/
 user_pref("geo.provider.network.url", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
 user_pref("geo.provider.network.logging.enabled", false);
 
-/***FIREFOX TELEMETRY AND ANNOYANCES***/
-user_pref("browser.privatebrowsing.vpnpromourl", "");
-user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+/** EXNTENSIONS ***/
+user_pref("extensions.blocklist.enabled", true);
+user_pref("extensions.webextensions.tabhide.enabled", false);
+
+/** TELEMETRY ***/
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("browser.ping-centre.telemetry", false);
 user_pref("app.normandy.enabled", false);
 user_pref("app.normandy.api_url", "");
 user_pref("toolkit.telemetry.unified", false);
 user_pref("toolkit.telemetry.enabled", false);
-user_pref("toolkit.telemetry.server", "data:,");
 user_pref("toolkit.telemetry.archive.enabled", false);
 user_pref("toolkit.telemetry.newProfilePing.enabled", false);
 user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
@@ -135,18 +207,38 @@ user_pref("toolkit.telemetry.coverage.opt-out", true);
 user_pref("toolkit.coverage.endpoint.base", "");
 user_pref("app.shield.optoutstudies.enabled", false);
 user_pref("browser.discovery.enabled", false);
-user_pref("extensions.getAddons.showPane", false);
+user_pref("breakpad.reportURL", "");
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.enabled", false);
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
 user_pref("default-browser-agent.enabled", false);
-user_pref("datareporting.policy.dataSubmissionEnabled", false);
-user_pref("datareporting.healthreport.uploadEnabled", false);
-user_pref("browser.ping-centre.telemetry", false);
-
 
 /****************************************************************************
- * START: PESKYFOX                                                          *
+ * SECTION: PESKYFOX                                                        *
 ****************************************************************************/
 
-/*** NEW TAB PAGE ***/
+/** MOZILLA UI ANNOYANCES ***/
+user_pref("browser.privatebrowsing.vpnpromourl", "");
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false); 
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false); 
+user_pref("extensions.getAddons.showPane", false);
+user_pref("security.dialog_enable_delay", 0);
+
+/** WARNINGS ***/
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnCloseOtherTabs", false);
+user_pref("browser.tabs.warnOnOpen", false);
+user_pref("browser.aboutConfig.showWarning", false);
+
+/** FULLSCREEN PREFS ***/
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("full-screen-api.warning.delay", 0);
+user_pref("full-screen-api.warning.timeout", 0);
+
+/** NEW TAB PAGE ***/
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.startup.page", 3);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
@@ -165,36 +257,20 @@ user_pref("browser.messaging-system.whatsNewPanel.enabled", false);
 
 /*** POCKET ***/
 user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.pocket.api"," ");
+user_pref("extensions.pocket.oAuthConsumerKey", " ");
+user_pref("extensions.pocket.site", " ");
 
-/*** DOWNLOADS ***/
+/** DOWNLOADS ***/
 user_pref("browser.download.useDownloadDir", true);
 user_pref("browser.download.manager.addToRecentDocs", false);
 user_pref("browser.download.hide_plugins_without_extensions", false);
 
-/*** WARNINGS ***/
-user_pref("browser.tabs.warnOnClose", false);
-user_pref("browser.tabs.warnOnCloseOtherTabs", false);
-user_pref("browser.tabs.warnOnOpen", false);
-user_pref("full-screen-api.warning.delay", -1);
-user_pref("full-screen-api.warning.timeout", -1);
-user_pref("browser.aboutConfig.showWarning", false);
-
-/*** AUTOFILL ***/
-user_pref("extensions.formautofill.addresses.enabled", false);
-user_pref("extensions.formautofill.available", "off");
-user_pref("extensions.formautofill.creditCards.available", false);
-user_pref("extensions.formautofill.creditCards.enabled", false);
-user_pref("extensions.formautofill.heuristics.enabled", false);
-user_pref("browser.formfill.enable", false);
-user_pref("signon.autofillForms", false);
-user_pref("signon.rememberSignons", false);
-user_pref("signon.formlessCapture.enabled", false);
-
-/*** ANNOYANCES ***/
+/** VARIOUS ***/
+user_pref("browser.tabs.unloadOnLowMemory", false);
 user_pref("browser.urlbar.suggest.engines", false);
-user_pref("browser.shell.checkDefaultBrowser", false);
 user_pref("permissions.default.desktop-notification", 2);
-user_pref("media.autoplay.default", 5);
+user_pref("media.autoplay.default", 1);
 user_pref("media.block-autoplay-until-in-foreground", true);
 user_pref("browser.backspace_action", 2);
 user_pref("ui.key.menuAccessKey", 0);
@@ -206,34 +282,72 @@ user_pref("browser.display.show_image_placeholders", false);
 user_pref("view_source.wrap_long_lines", true);
 user_pref("devtools.debugger.ui.editor-wrapping", true);
 
-/*** TAB BEHAVIOR ***/
-user_pref("browser.sessionstore.restore_on_demand", true);
+/** PDF ***/
+user_pref("pdfjs.disabled", false);
+user_pref("browser.helperApps.showOpenOptionForPdfJS", true);
+user_pref("pdfjs.defaultZoomValue", "page-width");
+
+/** TAB BEHAVIOR ***/
 user_pref("browser.link.open_newwindow", 3);
 user_pref("browser.link.open_newwindow.restriction", 0);
+user_pref("dom.disable_window_move_resize", true);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
 user_pref("browser.bookmarks.openInTabClosesMenu", false);
 user_pref("browser.tabs.loadBookmarksInBackground", true);
 user_pref("browser.tabs.loadBookmarksInTabs", true);
 user_pref("image.avif.enabled", true);
-user_pref("browser.helperApps.showOpenOptionForPdfJS", true); 
 user_pref("editor.truncate_user_pastes", false);
 user_pref("clipboard.plainTextOnly", true);
-user_pref("dom.disable_window_move_resize", true);
 user_pref("dom.popup_allowed_events", "click dblclick");
-
+user_pref("dom.disable_open_during_load", true);
 
 /****************************************************************************
- * START: DEV / NIGHTLY PREFS                                                 *
+ * SECTION: FIREFOX BETA & DEVELOPER                                        *
 ****************************************************************************/
-// user_pref("browser.startup.homepage.abouthome_cache.enabled", true);
-// user_pref("network.cookie.sameSite.laxByDefault", true);
-// user_pref("network.cookie.sameSite.noneRequiresSecure", true);
-// user_pref("network.cookie.sameSite.schemeful", true);
-// user_pref("layout.css.grid-template-masonry-value.enabled", true);
-// user_pref("layout.css.constructable-stylesheets.enabled", true);
-// user_pref("javascript.options.warp", true);
-// user_pref("dom.input_events.beforeinput.enabled", true);
-// user_pref("dom.forms.inputmode", true);
+/**
+*
+* user_pref("browser.startup.homepage.abouthome_cache.enabled", true);
+* user_pref("layout.css.focus-visible.enabled", true);
+* user_pref("layout.css.grid-template-masonry-value.enabled", true);
+* user_pref("layout.css.constructable-stylesheets.enabled", true);
+* user_pref("media.videocontrols.picture-in-picture.allow-multiple", true);
+* user_pref("dom.input_events.beforeinput.enabled", true);
+* user_pref("dom.forms.inputmode", true);
+*
+**/
+
+/****************************************************************************
+ * SECTION: FIREFOX NIGHTLY                                                 *
+****************************************************************************/
+/**
+* // PREF: Mozilla Translate
+* user_pref("browser.translation.ui.show", true);
+* user_pref("browser.translation.detectLanguage", true);
+*
+* // PREF: HTTP3 / QUIC
+* user_pref("network.http.http3.enabled", true);
+*
+* // PREF: Samesite Cookie defaults
+* user_pref("network.cookie.sameSite.laxByDefault", true);
+* user_pref("network.cookie.sameSite.noneRequiresSecure", true);
+* user_pref("network.cookie.sameSite.schemeful", true);
+*
+* // PREF: Cookie isolation changes?
+* // https://bugzilla.mozilla.org/show_bug.cgi?id=1685575
+* // https://hg.mozilla.org/mozilla-central/rev/37acd60f7bcc15481e8ebb231bb2f80fe9fd0a2e
+* user_pref("browser.contentblocking.state-partitioning.mvp.ui.enabled", true);
+* // https://www.reddit.com/r/firefox/comments/kzrmsb/enable_dynamic_first_party_isolation/gjtxyje/?context=3
+* user_pref("browser.contentblocking.reject-and-isolate-cookies.preferences.ui.enabled", true);
+*
+* // PREF: Evolution of ESNI for DNS-over-HTTPS
+* user_pref("network.dns.echconfig.enabled", true);
+* user_pref("network.dns.use_https_rr_as_altsvc", true);
+*
+* // PREF: Proton UI Redesign
+* user_pref("browser.proton.enabled", true);
+* user_pref("browser.proton.tabs.enabled", true);
+*
+**/
 
 /****************************************************************************
  * END: BETTERFOX                                                           *
