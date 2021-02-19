28275ea2644b21b616522a51881820899bf8d819
yokoffing <hjstephens09@gmail.com>
2021-02-19 16:27:14 -0500

    hyperlinks

--- FastFox.js
+++ FastFox.js
@@ -28,8 +28,8 @@
 // [3] https://www.reddit.com/r/firefox/comments/fo1jwz/make_firefox_faster/flhh5l2/
 // user_pref("gfx.webrender.all", true);
 // If your hardware doesn't support Webrender, you can fallback to Webrender's software renderer.
-// Both preferences need to be "true" if you're using the software renderer.
-// https://www.ghacks.net/2020/12/14/how-to-find-out-if-webrender-is-enabled-in-firefox-and-how-to-enable-it-if-it-is-not/
+// [NOTE] Both preferences need to be "true" if you're using the software renderer.
+// [1] https://www.ghacks.net/2020/12/14/how-to-find-out-if-webrender-is-enabled-in-firefox-and-how-to-enable-it-if-it-is-not/
 // user_pref("gfx.webrender.software", true);
 
 // PREF: Lazy Image Loading
@@ -52,12 +52,12 @@ user_pref("browser.startup.preXulSkeletonUI", false);
 // it will also use more memory. You can reduce the number of content processes if your
 // computer is running out of memory. If your computer's system information shows more
 // than 8 GB of RAM, you would likely benefit from a high content process limit.
-// https://support.mozilla.org/en-US/kb/performance-settings
+// [1] https://support.mozilla.org/en-US/kb/performance-settings
 // default = 8. -1 allows Firefox to use as many as it wants
 // user_pref("dom.ipc.processCount", 8);
 
 // PREF: Prevent FF from going offline
-// https://lifehacker.com/stop-firefox-from-automatically-entering-work-offline-5714560
+// [1] https://lifehacker.com/stop-firefox-from-automatically-entering-work-offline-5714560
 // user_pref("network.manage-offline-status", false);
 
 // PREF: Disable Firefox animations 
