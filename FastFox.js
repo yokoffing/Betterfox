9d1372cfdd60f5e26cf44ce37557f1cabec27599
yokoffing <hjstephens09@gmail.com>
2021-02-19 16:25:41 -0500

    Remove Warp options (no longer in Firefox)

--- FastFox.js
+++ FastFox.js
@@ -11,7 +11,7 @@
  * FasterFox                                                                *
  * "Non ducor duco."                                                        *
  * priority: speedy browsing                                                *
- * version: 29 January 2021                                                 *
+ * version: 19 February 2021                                                *
  * url: https://github.com/yokoffing/Better-Fox                             *
  ***************************************************************************/
 
@@ -22,19 +22,15 @@
  *                                                                          *
 ****************************************************************************/
 
-// PREF: Activate Webrender, Firefox's new rendering engine
-// https://hacks.mozilla.org/2017/10/the-whole-web-at-maximum-fps-how-webrender-gets-rid-of-jank/
-// https://wiki.mozilla.org/Platform/GFX/WebRender_Where
-// https://www.reddit.com/r/firefox/comments/fo1jwz/make_firefox_faster/flhh5l2/
-user_pref("gfx.webrender.all", true);
+// PREF: Force enable all Webrender prefs
+// [1] https://hacks.mozilla.org/2017/10/the-whole-web-at-maximum-fps-how-webrender-gets-rid-of-jank/
+// [2] https://wiki.mozilla.org/Platform/GFX/WebRender_Where
+// [3] https://www.reddit.com/r/firefox/comments/fo1jwz/make_firefox_faster/flhh5l2/
+// user_pref("gfx.webrender.all", true);
 // If your hardware doesn't support Webrender, you can fallback to Webrender's software renderer.
 // Both preferences need to be "true" if you're using the software renderer.
 // https://www.ghacks.net/2020/12/14/how-to-find-out-if-webrender-is-enabled-in-firefox-and-how-to-enable-it-if-it-is-not/
 // user_pref("gfx.webrender.software", true);
-
-// PREF: Improvements to the javascript engine
-// https://hacks.mozilla.org/2020/11/warp-improved-js-performance-in-firefox-83/
-user_pref("javascript.options.warp", true); /* default */
 
 // PREF: Lazy Image Loading
 // https://www.ghacks.net/2020/02/15/firefox-75-gets-lazy-loading-support-for-images/
@@ -57,8 +53,8 @@ user_pref("browser.startup.preXulSkeletonUI", false);
 // computer is running out of memory. If your computer's system information shows more
 // than 8 GB of RAM, you would likely benefit from a high content process limit.
 // https://support.mozilla.org/en-US/kb/performance-settings
-// default = 8
-// user_pref("dom.ipc.processCount", 16);
+// default = 8. -1 allows Firefox to use as many as it wants
+// user_pref("dom.ipc.processCount", 8);
 
 // PREF: Prevent FF from going offline
 // https://lifehacker.com/stop-firefox-from-automatically-entering-work-offline-5714560
