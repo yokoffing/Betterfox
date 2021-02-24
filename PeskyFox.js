a8f995855b94950f474299959103ccf5c57ed2dc
yokoffing <hjstephens09@gmail.com>
2021-02-24 11:33:04 -0500

    add default tag

--- PeskyFox.js
+++ PeskyFox.js
@@ -231,7 +231,7 @@ user_pref("view_source.wrap_long_lines", true);
 user_pref("devtools.debugger.ui.editor-wrapping", true);
 
 // PREF: print preview
-user_pref("print.tab_modal.enabled", true);
+user_pref("print.tab_modal.enabled", true); // default
 
 /****************************************************************************
  * SECTION: PDF                                                             *
