0bc41168b0f6a2487c848e1e8d766fdb46ed140c
yokoffing <hjstephens09@gmail.com>
2021-08-04 18:36:05 -0400

    add ECH

--- user.js
+++ user.js
@@ -78,6 +78,10 @@ user_pref("dom.security.https_only_mode.upgrade_local", true);
 user_pref("network.trr.send_user-agent_headers", false);
 user_pref("network.dns.skipTRR-when-parental-control-enabled", false);
 
+/** ESNI / ECH ***/
+user_pref("network.dns.echconfig.enabled", true);
+user_pref("network.dns.use_https_rr_as_altsvc", true);
+
 /** PASSWORDS AND AUTOFILL ***/
 user_pref("signon.autofillForms.http", false);
 user_pref("security.insecure_field_warning.contextual.enabled", true);
