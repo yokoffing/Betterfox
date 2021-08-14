3827c4f34681ee6f8c65abb3390425c23e3b4d8b
yokoffing <hjstephens09@gmail.com>
2021-08-14 12:14:21 -0400

    remove Windows SSO (for now)

--- user.js
+++ user.js
@@ -128,7 +128,6 @@ user_pref("browser.formfill.enable", false);
 
 /** MIXED CONTENT + CROSS-SITE ***/
 user_pref("network.auth.subresource-http-auth-allow", 1);
-user_pref("network.http.windows-sso.enabled", false);
 user_pref("security.mixed_content.upgrade_display_content", true);
 user_pref("dom.block_download_insecure", true);
 user_pref("pdfjs.enableScripting", false);
