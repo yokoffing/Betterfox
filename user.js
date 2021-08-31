cfe50502ef339bd85dc8a1c3e9b2a7adc698360e
yokoffing <hjstephens09@gmail.com>
2021-08-31 19:22:18 -0400

    spec. con. revert

--- user.js
+++ user.js
@@ -55,10 +55,13 @@ user_pref("privacy.sanitize.timeSpan", 0);
 user_pref("privacy.history.custom", true);
 
 /** SPECULATIVE CONNECTIONS ***/
-user_pref("network.predictor.enable-hover-on-ssl", true);
-user_pref("network.predictor.enable-prefetch", true);
+user_pref("network.predictor.enabled", false);
 user_pref("network.dns.disablePrefetch", true);
-user_pref("network.dns.disablePrefetchFromHTTPS", false);
+user_pref("network.dns.disablePrefetchFromHTTPS", true);
+user_pref("browser.urlbar.speculativeConnect.enabled", false);
+user_pref("network.prefetch-next", false);
+user_pref("network.http.speculative-parallel-limit", 0);
+user_pref("network.preload", false);
 
 /** SEARCH / URL BAR ***/
 user_pref("browser.search.separatePrivateDefault", true);
