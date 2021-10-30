05e4ba504bf0af948c587a178b9445d824d12223
yokoffing <hjstephens09@gmail.com>
2021-10-30 14:25:45 -0400

    Firefox Suggest

--- user.js
+++ user.js
@@ -68,6 +68,8 @@ user_pref("browser.search.separatePrivateDefault", true);
 user_pref("browser.search.separatePrivateDefault.ui.enabled", true);
 user_pref("browser.search.suggest.enabled", false);
 user_pref("browser.urlbar.groupLabels.enabled", false);
+user_pref("browser.urlbar.suggest.quicksuggest", false);
+user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
 user_pref("browser.fixup.alternate.enabled", false);
 user_pref("security.insecure_connection_text.enabled", true);
 user_pref("security.insecure_connection_text.pbmode.enabled", true);
