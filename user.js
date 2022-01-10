0889a4bc96052c7c3883cf2d173f6208a040c4ab
yokoffing <hjstephens09@gmail.com>
2022-01-09 20:31:28 -0500

    dom.forms.inputmode now default

--- user.js
+++ user.js
@@ -264,7 +264,6 @@ user_pref("view_source.wrap_long_lines", true);
 user_pref("devtools.debugger.ui.editor-wrapping", true);
 user_pref("layout.css.constructable-stylesheets.enabled", true);
 user_pref("layout.css.grid-template-masonry-value.enabled", true);
-user_pref("dom.forms.inputmode", true);
 
 /** TAB BEHAVIOR ***/
 user_pref("dom.disable_window_move_resize", true);
