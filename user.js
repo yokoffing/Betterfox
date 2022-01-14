fc0318a3db0de79e99ea118331cda01118a53e29
yokoffing <hjstephens09@gmail.com>
2022-01-14 13:16:06 -0500

    remove require safe nego

--- user.js
+++ user.js
@@ -44,7 +44,6 @@ user_pref("security.remote_settings.crlite_filters.enabled", true);
 
 /** SSL / TLS ***/
 user_pref("security.ssl.treat_unsafe_negotiation_as_broken", true);
-user_pref("security.ssl.require_safe_negotiation", true);
 user_pref("browser.xul.error_pages.expert_bad_cert", true);
 user_pref("browser.ssl_override_behavior", 1);
 user_pref("security.tls.enable_0rtt_data", false);
