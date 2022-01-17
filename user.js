fbc0c7f54844c2f48f34f420102637c998c97444
yokoffing <hjstephens09@gmail.com>
2022-01-17 14:41:07 -0500

    Update user.js

--- user.js
+++ user.js
@@ -251,6 +251,7 @@ user_pref("extensions.pocket.site", " ");
 
 /** DOWNLOADS ***/
 user_pref("browser.download.useDownloadDir", false);
+user_pref("browser.download.alwaysOpenPanel", false);
 user_pref("browser.download.manager.addToRecentDocs", false);
 
 /** VARIOUS ***/
