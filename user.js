5355e391cf2acdeebf35df83101f8db1caddea77
yokoffing <hjstephens09@gmail.com>
2022-01-29 11:30:03 -0500

    TP is default

--- user.js
+++ user.js
@@ -28,7 +28,6 @@ user_pref("browser.startup.homepage.abouthome_cache.enabled", true);
 ****************************************************************************/
 /** TRACKING PROTECTION ***/
 user_pref("browser.contentblocking.category", "strict");
-user_pref("privacy.trackingprotection.enabled", true);
 user_pref("privacy.trackingprotection.lower_network_priority", true);
 user_pref("privacy.partition.network_state.ocsp_cache", true);
 user_pref("privacy.partition.serviceWorkers", true);
