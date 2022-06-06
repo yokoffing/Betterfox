c633abcbed12678088ec0e97ac75d21ce035d09e
yokoffing <hjstephens09@gmail.com>
2022-06-06 15:51:22 -0400

    Update user.js

--- user.js
+++ user.js
@@ -95,7 +95,6 @@ user_pref("network.IDN_show_punycode", true);
 
 /** HTTPS-ONLY MODE ***/
 user_pref("dom.security.https_only_mode", true);
-user_pref("dom.security.https_only_mode_ever_enabled", true);
 user_pref("dom.security.https_only_mode_error_page_user_suggestions", true);
 
 /** DNS-over-HTTPS (DOH) ***/
