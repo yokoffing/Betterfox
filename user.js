ce2e881c44ab1af1b5f0f4944ae3844c02f99e99
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-07-31 16:05:31 -0400

    user_pref("browser.fixup.alternate.enabled", false); default in FF104+

--- user.js
+++ user.js
@@ -79,7 +79,6 @@ user_pref("browser.search.suggest.enabled", false);
 user_pref("browser.urlbar.groupLabels.enabled", false);
 user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
 user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
-user_pref("browser.fixup.alternate.enabled", false);
 user_pref("security.insecure_connection_text.enabled", true);
 user_pref("security.insecure_connection_text.pbmode.enabled", true);
 user_pref("network.IDN_show_punycode", true);
