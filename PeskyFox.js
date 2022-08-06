7eb9a4860bb8af7f89bb6d6739ff34c2d3478daf
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-08-05 22:57:34 -0400

    grammar

--- PeskyFox.js
+++ PeskyFox.js
@@ -205,7 +205,7 @@ user_pref("browser.urlbar.suggest.topsites", false);
 // user_pref("browser.urlbar.autoFill", true); [DEFAULT]
 // user_pref("browser.urlbar.autoFill.adaptiveHistory.enabled", false);
 
-PREF: Quick Actions in the URL Bar
+// PREF: Quick Actions in the URL Bar
 // [1] https://www.ghacks.net/2022/07/19/mozilla-is-testing-quick-actions-in-firefoxs-address-bar/
 // user_pref("browser.urlbar.quickactions.enabled", false);
 // user_pref("browser.urlbar.shortcuts.quickactions", false);
