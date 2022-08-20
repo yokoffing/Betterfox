fec43abc00a27a1c4a7eefd1555c45eab813cc16
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-08-20 14:27:56 -0400

    Update SmoothFox.js

--- SmoothFox.js
+++ SmoothFox.js
@@ -16,14 +16,27 @@
  ***************************************************************************/
 
 /****************************************************************************
- * OPTION 1: SMOOTH SCROLLING                                               *
+ * OPTION 1: INSTANT SCROLLING (SIMPLE ADJUSTMENT)                          *
+****************************************************************************/
+user_pref("general.smoothScroll", true); // DEFAULT
+user_pref("mousewheel.default.delta_multiplier_y", 300);
+
+/****************************************************************************
+ * OPTION 2: SMOOTH SCROLLING                                               *
 ****************************************************************************/
 user_pref("general.smoothScroll", true); // DEFAULT
 user_pref("mousewheel.default.delta_multiplier_y", 250);
 user_pref("general.smoothScroll.msdPhysics.enabled", true);
 
 /****************************************************************************
- * OPTION 2: INSTANT SCROLLING (SIMPLE ADJUSTMENT)                          *
+ * OPTION 3: EDGE-LIKE SMOOTH SCROLLING                                     *
 ****************************************************************************/
 user_pref("general.smoothScroll", true); // DEFAULT
-user_pref("mousewheel.default.delta_multiplier_y", 300);
+user_pref("mousewheel.default.delta_multiplier_y", 250);
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 250);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 400);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 400);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 120);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", 0.4);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 5000);
