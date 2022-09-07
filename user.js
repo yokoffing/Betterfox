01b4d10b0d123347c8d71d7e0b1b5d127c655b8f
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-09-07 15:39:03 -0400

    user_pref("privacy.partition.serviceWorkers", true); now DEFAULT

--- user.js
+++ user.js
@@ -35,7 +35,6 @@ user_pref("privacy.trackingprotection.emailtracking.enabled", true);
 user_pref("urlclassifier.trackingSkipURLs", "*.reddit.com, *.twitter.com, *.twimg.com");
 user_pref("urlclassifier.features.socialtracking.skipURLs", "*.instagram.com, *.twitter.com, *.twimg.com");
 /* Use Privacy Essentails list for social tracking: https://github.com/yokoffing/filterlists/blob/main/PrivacyEssentials.txt */
-user_pref("privacy.partition.serviceWorkers", true);
 user_pref("privacy.partition.bloburl_per_agent_cluster", true);
 user_pref("privacy.partition.always_partition_third_party_non_cookie_storage", true);
 user_pref("beacon.enabled", false);
