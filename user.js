ada3d2556356eea0826d9c836674986c981cf169
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-09-24 12:23:49 -0400

    lower network priority of trackers

--- user.js
+++ user.js
@@ -37,6 +37,7 @@ user_pref("urlclassifier.trackingSkipURLs", "*.reddit.com, *.twitter.com, *.twim
 user_pref("urlclassifier.features.socialtracking.skipURLs", "*.instagram.com, *.twitter.com, *.twimg.com");
 /** Use Privacy Essentails list for social tracking protection:
 * https://github.com/yokoffing/filterlists/blob/main/PrivacyEssentials.txt ***/
+user_pref("privacy.trackingprotection.lower_network_priority", true);
 user_pref("privacy.partition.bloburl_per_agent_cluster", true);
 user_pref("privacy.partition.always_partition_third_party_non_cookie_storage", true);
 user_pref("beacon.enabled", false);
