2bb68423d4da54513ab236ee9f0bc39c2c040df2
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-09-29 14:36:45 -0400

    note for smoothfox

--- user.js
+++ user.js
@@ -248,23 +248,12 @@ user_pref("dom.popup_allowed_events", "click dblclick");
 /****************************************************************************
  * SECTION: SMOOTHFOX                                                       *
 ****************************************************************************/
-/** Uncomment each pref to enable ***/
-
-// user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
-// user_pref("general.smoothScroll.msdPhysics.enabled", true);
-// user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600);
-// user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 650);
-// user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 25);
-// user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", 2.0);
-// user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 250);
-// user_pref("general.smoothScroll.currentVelocityWeighting", 1.0);
-// user_pref("general.smoothScroll.stopDecelerationWeighting", 1.0);
-// user_pref("mousewheel.default.delta_multiplier_y", 280);
+// see https://github.com/yokoffing/BetterFox/blob/master/SmoothFox.js
 
 /****************************************************************************
  * START: MY OVERRIDES                                                      *
 ****************************************************************************/
-/** Enter your personal prefs below this line ***/
+// Enter your personal prefs below this line:
 
 /****************************************************************************
  * END: BETTERFOX                                                           *
