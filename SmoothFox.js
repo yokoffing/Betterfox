bb56d739b3aab2af4c348f446b11ac51aa53ccc0
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-09-29 14:38:19 -0400

    SmoothFox Sep 2022 (#73)
    * remove option 4
    
    * remove option 3

--- SmoothFox.js
+++ SmoothFox.js
@@ -11,54 +11,32 @@
  * SmoothFox                                                                *
  * "Faber est suae quisque fortunae"                                        *
  * priority: better scrolling                                               *
- * version: September 2022                                                  *
+ * version: October 2022                                                    *
  * url: https://github.com/yokoffing/Better-Fox                             *
  ***************************************************************************/
 
 /****************************************************************************
  * OPTION 1: INSTANT SCROLLING (SIMPLE ADJUSTMENT)                          *
 ****************************************************************************/
+// recommended for 60hz displays
 user_pref("general.smoothScroll", true); // DEFAULT
-user_pref("mousewheel.default.delta_multiplier_y", 300);  // 250-500
+user_pref("mousewheel.default.delta_multiplier_y", 275);  // 250-500
 
 /****************************************************************************
  * OPTION 2: SMOOTH SCROLLING                                               *
 ****************************************************************************/
+// recommended for 90hz+ displays
 user_pref("general.smoothScroll", true); // DEFAULT
 user_pref("mousewheel.default.delta_multiplier_y", 250);
 user_pref("general.smoothScroll.msdPhysics.enabled", true);
 
 /****************************************************************************
- * OPTION 3: EDGE-LIKE SMOOTH SCROLLING                                     *
-****************************************************************************/
-user_pref("general.smoothScroll", true); // DEFAULT
-user_pref("mousewheel.default.delta_multiplier_y", 250);
-user_pref("general.smoothScroll.msdPhysics.enabled", true);
-user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 250);
-user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 400);
-user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 400);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 120);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", 0.4);
-user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 5000);
-
-/****************************************************************************
- * OPTION 4: NATURAL SMOOTH SCROLLING V4                                    *
+ * OPTION 3: NATURAL SMOOTH SCROLLING [MODIFIED]                            *
 ****************************************************************************/
 // from https://github.com/AveYo/fox/blob/main/Natural%20Smooth%20Scrolling%20for%20user.js
-user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS",   12);
-user_pref("general.smoothScroll.msdPhysics.enabled",                    true);
-user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",   200);
-user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",       250);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",           25);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",     2.0);
-user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",      250);
-user_pref("general.smoothScroll.currentVelocityWeighting",             1.0);
-user_pref("general.smoothScroll.stopDecelerationWeighting",            1.0);
-
-/****************************************************************************
- * OPTION 5: NATURAL SMOOTH SCROLLING [MODIFIED]                            *
-****************************************************************************/
-// Largely matches Chrome flags: Windows Scrolling Personality and Smooth Scrolling
+// largely matches Chrome flags: Windows Scrolling Personality and Smooth Scrolling
+// recommended for 90hz+ displays
+user_pref("general.smoothScroll", true); // DEFAULT
 user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
 user_pref("general.smoothScroll.msdPhysics.enabled", true);
 user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600); // 200
