f0384623a0c6d670c1c1ff408610a1d9364c7f58
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-09-30 14:26:47 -0400

    overrides Sept 2022 (#76)
    * ui.key.menuAccessKey
    
    * devtools.popups.debug

--- user-overrides.js
+++ user-overrides.js
@@ -2,8 +2,7 @@
 
 /****************************************************************************
  *                                                                         *
- *  [NOTE] These are a backup of personal prefs.                           *
- *  Do not copy+paste unless you know what you're doing.                   *
+ *  [NOTE] This list is a backup of personal prefs                         *
  *                                                                         *
 ****************************************************************************/
 
@@ -20,11 +19,15 @@ user_pref("reader.parse-on-load.enabled", false); // disable reader mode
 /** SECUREFOX ***/
 user_pref("browser.search.separatePrivateDefault", false); // disable separate search engine
 user_pref("browser.search.separatePrivateDefault.ui.enabled", false); // disable separate search engine
+user_pref("browser.urlbar.showSearchSuggestionsFirst", false); // unselect "Show search suggestions ahead of browsing history in address bar results" for clean UI
 user_pref("signon.management.page.breach-alerts.enabled", false); // extra hardening
+user_pref("signon.generation.enabled", false); // unselect "Suggest and generate strong passwords" for clean UI
 user_pref("privacy.userContext.enabled", false); // disable Containers
 user_pref("privacy.sanitize.sanitizeOnShutdown", true); // clear browsing data on shutdown
 user_pref("privacy.clearOnShutdown.offlineApps", true); // clear browsing data on shutdown
 user_pref("dom.security.https_only_fire_http_request_background_timer_ms", 1000); // decrease timer for HTTP background request
+// user_pref("media.eme.enabled", false); // disable all DRM content
+// user_pref("browser.eme.ui.enabled", false); // DRM cotent: hide the setting which also disables the webpage DRM prompt
 user_pref("browser.safebrowsing.provider.google4.gethashURL", ""); // extra hardening
 user_pref("browser.safebrowsing.provider.google4.updateURL", ""); // extra hardening
 user_pref("browser.safebrowsing.provider.google.gethashURL", ""); // extra hardening
@@ -32,8 +35,9 @@ user_pref("browser.safebrowsing.provider.google.updateURL", ""); // extra harden
 user_pref("browser.safebrowsing.downloads.remote.url", ""); // extra hardening
 
 /** PESKYFOX ***/
-/* user_pref("browser.urlbar.openintab", true); */ // spawn new tab when typing in URL bar, instead of using current tab
+// user_pref("browser.urlbar.openintab", true); // spawn new tab when typing in URL bar, instead of using current tab
 user_pref("browser.toolbars.bookmarks.visibility", "never"); // always hide bookmark bar
+user_pref("gfx.webrender.quality.force-subpixel-aa-where-possible", true); // font improvement
 user_pref("browser.urlbar.suggest.bookmark", false); // hide URL bar dropdown suggestions
 user_pref("browser.urlbar.suggest.engines", false); // hide URL bar dropdown suggestions
 user_pref("browser.urlbar.suggest.history", false); // hide URL bar dropdown suggestions
@@ -43,11 +47,18 @@ user_pref("browser.urlbar.maxRichResults", 1); // minimum suggestion needed for 
 user_pref("browser.bookmarks.max_backups", 2); // minimize disk use
 user_pref("view_source.wrap_long_lines", true); // wrap source lines
 user_pref("devtools.debugger.ui.editor-wrapping", true); // wrap lines in devtools
+user_pref("devtools.popups.debug", true); // Auto-open DevTools for popups
 user_pref("reader.color_scheme", "auto"); // match system theme for when reader is enabled
 user_pref("browser.zoom.full", false); // text-only zoom, not all elements on page
+user_pref("ui.key.menuAccessKey", 0); // remove underlined characters from various settings
 
-/** MY FONT ***/
-user_pref("font.name.monospace.x-western", "Cascadia Mono"); // preferred font
-user_pref("gfx.webrender.quality.force-subpixel-aa-where-possible", true); // font improvement
-user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5); // font improvement [WINDOWS-only?]
-user_pref("gfx.font_rendering.cleartype_params.force_gdi_classic_for_families", " "); // font improvement [WINDOWS-only?]
+// Custom monospace font
+// Enforce one custom monospace font (or neither one!) depending on the machine:
+// user_pref("font.name.monospace.x-western", "SF Mono"); // preferred font [macOS]
+// user_pref("font.name.monospace.x-western", "Cascadia Mono"); // preferred font [WINDOWS]
+// then add:
+// user_pref("layout.css.font-visibility.trackingprotection", 3); // breaks using custom font on pages
+
+// WINDOWS-only prefs; uncomment to enable:
+// user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5); // font improvement [WINDOWS-only]
+// user_pref("gfx.font_rendering.cleartype_params.force_gdi_classic_for_families", " "); // font improvement [WINDOWS-only]
