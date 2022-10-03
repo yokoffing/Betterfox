9aee03b30d38e4eb454785858882d6f03485ebfc
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-10-03 01:17:26 -0400

    browser.cache.disk.enable

--- user-overrides.js
+++ user-overrides.js
@@ -23,16 +23,16 @@ user_pref("browser.urlbar.showSearchSuggestionsFirst", false); // unselect "Show
 user_pref("signon.management.page.breach-alerts.enabled", false); // extra hardening
 user_pref("signon.generation.enabled", false); // unselect "Suggest and generate strong passwords" for clean UI
 user_pref("privacy.userContext.enabled", false); // disable Containers
+    //user_pref("browser.cache.disk.enable", true); // re-enable disk cache; more efficient to keep the browser cache; cleared on shutdown
 user_pref("privacy.sanitize.sanitizeOnShutdown", true); // clear browsing data on shutdown
 user_pref("privacy.clearOnShutdown.offlineApps", true); // clear browsing data on shutdown
 user_pref("dom.security.https_only_fire_http_request_background_timer_ms", 1000); // decrease timer for HTTP background request
-// user_pref("media.eme.enabled", false); // disable all DRM content
-// user_pref("browser.eme.ui.enabled", false); // DRM cotent: hide the setting which also disables the webpage DRM prompt
+    //user_pref("media.eme.enabled", false); // disable all DRM content
+    //user_pref("browser.eme.ui.enabled", false); // DRM cotent: hide the setting which also disables the webpage DRM prompt
 user_pref("browser.safebrowsing.provider.google4.gethashURL", ""); // extra hardening
 user_pref("browser.safebrowsing.provider.google4.updateURL", ""); // extra hardening
 user_pref("browser.safebrowsing.provider.google.gethashURL", ""); // extra hardening
 user_pref("browser.safebrowsing.provider.google.updateURL", ""); // extra hardening
-user_pref("browser.safebrowsing.downloads.remote.url", ""); // extra hardening
 
 /** PESKYFOX ***/
 user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false); // Settings>Home>Firefox Home Content>Recent Activity>Shortcuts>Sponsored shortcuts
@@ -40,7 +40,7 @@ user_pref("browser.newtabpage.activity-stream.showSponsored", false); //  Settin
 user_pref("browser.newtabpage.activity-stream.section.highlights.includeBookmarks", false); // Settings>Home>Firefox Home Content>Recent Activity>Bookmarks
 user_pref("browser.newtabpage.activity-stream.section.highlights.includeDownloads", false); // Settings>Home>Firefox Home Content>Recent Activity>Most Recent Download
 user_pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", false); // Settings>Home>Firefox Home Content>Recent Activity>Visited Pages
-// user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false); // Settings>Home>Firefox Home Content>Recent Activity>Pages Saved to Pocket
+    //user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false); // Settings>Home>Firefox Home Content>Recent Activity>Pages Saved to Pocket
 user_pref("browser.toolbars.bookmarks.visibility", "never"); // always hide bookmark bar
 user_pref("gfx.webrender.quality.force-subpixel-aa-where-possible", true); // font improvement
 user_pref("browser.urlbar.suggest.bookmark", false); // hide URL bar dropdown suggestions
@@ -55,15 +55,15 @@ user_pref("devtools.debugger.ui.editor-wrapping", true); // wrap lines in devtoo
 user_pref("reader.color_scheme", "auto"); // match system theme for when reader is enabled
 user_pref("browser.zoom.full", false); // text-only zoom, not all elements on page
 user_pref("ui.key.menuAccessKey", 0); // remove underlined characters from various settings
-// user_pref("browser.urlbar.openintab", true); // spawn new tab when typing in URL bar, instead of using current tab
+    //user_pref("browser.urlbar.openintab", true); // spawn new tab when typing in URL bar, instead of using current tab
 
 // Custom monospace font
 // Enforce one custom monospace font (or neither one!) depending on the machine:
-// user_pref("font.name.monospace.x-western", "SF Mono"); // preferred font [macOS]
-// user_pref("font.name.monospace.x-western", "Cascadia Mono"); // preferred font [WINDOWS]
+//user_pref("font.name.monospace.x-western", "SF Mono"); // preferred font [macOS]
+//user_pref("font.name.monospace.x-western", "Cascadia Mono"); // preferred font [WINDOWS]
 // then add:
-// user_pref("layout.css.font-visibility.trackingprotection", 3); // breaks using custom font on pages
+//user_pref("layout.css.font-visibility.trackingprotection", 3); // breaks using custom font on pages
 
 // WINDOWS-only prefs; uncomment to enable:
-// user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5); // font improvement [WINDOWS-only]
-// user_pref("gfx.font_rendering.cleartype_params.force_gdi_classic_for_families", " "); // font improvement [WINDOWS-only]
+//user_pref("gfx.font_rendering.cleartype_params.rendering_mode", 5); // font improvement [WINDOWS-only]
+//user_pref("gfx.font_rendering.cleartype_params.force_gdi_classic_for_families", " "); // font improvement [WINDOWS-only]
