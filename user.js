b10c55c45b2de09563dc67536dab24de794eca3f
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-10-03 01:22:33 -0400

    user_pref("browser.sessionstore.interval", 30000);

--- user.js
+++ user.js
@@ -68,6 +68,7 @@ user_pref("browser.privatebrowsing.forceMediaMemoryCache", true);
 user_pref("media.memory_cache_max_size", 65536);
 user_pref("browser.sessionstore.privacy_level", 2);
 user_pref("browser.pagethumbnails.capturing_disabled", true);
+user_pref("browser.sessionstore.interval", 30000);
 
 /** SPECULATIVE CONNECTIONS ***/
 user_pref("browser.newtab.preload", false);
