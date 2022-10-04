6f23c145320656d94d853fa42b86692add2d0b85
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-10-03 21:28:46 -0400

    font spacing

--- user-overrides.js
+++ user-overrides.js
@@ -11,31 +11,31 @@
 /** Enter your personal prefs below this line ***/
 
 /** FASTFOX ***/
-user_pref("browser.sessionstore.restore_pinned_tabs_on_demand",             true);  // only load pinned tabs once selected
-user_pref("reader.parse-on-load.enabled",                                   false); // disable reader mode
+user_pref("browser.sessionstore.restore_pinned_tabs_on_demand",             			true);  // only load pinned tabs once selected
+user_pref("reader.parse-on-load.enabled",                                   			false); // disable reader mode
 
 /** SECUREFOX ***/
-user_pref("browser.search.separatePrivateDefault",                          false); // disable separate search engine
-user_pref("browser.search.separatePrivateDefault.ui.enabled",               false); // disable separate search engine
-user_pref("browser.urlbar.showSearchSuggestionsFirst",                      false); // unselect "Show search suggestions ahead of browsing history in address bar results" for clean UI
-user_pref("signon.management.page.breach-alerts.enabled",                   false); // extra hardening
-user_pref("signon.generation.enabled",                                      false); // unselect "Suggest and generate strong passwords" for clean UI
-user_pref("privacy.userContext.enabled",                                    false); // disable Containers
-//user_pref("browser.cache.disk.enable",                                    true);  // re-enable disk cache for lower-end PC; more efficient to keep the browser cache; cleared on shutdown
-//user_pref("media.cache_readahead_limit",                                  99999); // playback for higher-end PC
-//user_pref("media.cache_resume_threshold",                                 99999); // playback for higher-end PC
-//user_pref("media.cache_size",                                             2048000); // playback for higher-end PC
-//user_pref("media.memory_cache_max_size",                                  512000); // memory cache; playback for higher-end PC
-//user_pref("media.memory_caches_combined_limit_kb",                        2560000); // memory cache; playback for higher-end PC
-user_pref("privacy.sanitize.sanitizeOnShutdown",                            true);  // clear browsing data on shutdown      //  REMOVE WHEN NOV. UPDATE IS PUSHED
-user_pref("privacy.clearOnShutdown.offlineApps",                            true);  // disable if login issue after restart; localStorage, service worker cache, QuotaManager (IndexedDB, asm-cache)
-user_pref("dom.security.https_only_fire_http_request_background_timer_ms",  1000); // decrease timer for HTTP background request
-//user_pref("media.eme.enabled",                                            false); // disable all DRM content
-//user_pref("browser.eme.ui.enabled",                                       false); // DRM cotent: hide the setting which also disables the webpage DRM prompt
-user_pref("browser.safebrowsing.provider.google4.gethashURL",               ""); // extra hardening
-user_pref("browser.safebrowsing.provider.google4.updateURL",                ""); // extra hardening
-user_pref("browser.safebrowsing.provider.google.gethashURL",                ""); // extra hardening
-user_pref("browser.safebrowsing.provider.google.updateURL",                 ""); // extra hardening
+user_pref("browser.search.separatePrivateDefault",                          			false); // disable separate search engine
+user_pref("browser.search.separatePrivateDefault.ui.enabled",               			false); // disable separate search engine
+user_pref("browser.urlbar.showSearchSuggestionsFirst",                      			false); // unselect "Show search suggestions ahead of browsing history in address bar results" for clean UI
+user_pref("signon.management.page.breach-alerts.enabled",                   			false); // extra hardening
+user_pref("signon.generation.enabled",                                      			false); // unselect "Suggest and generate strong passwords" for clean UI
+user_pref("privacy.userContext.enabled",                                    			false); // disable Containers
+//user_pref("browser.cache.disk.enable",                                    			true);  // re-enable disk cache for lower-end PC; more efficient to keep the browser cache; cleared on shutdown
+//user_pref("media.cache_readahead_limit",                                  			99999); // playback for higher-end PC
+//user_pref("media.cache_resume_threshold",                                 			99999); // playback for higher-end PC
+//user_pref("media.cache_size",                                             			2048000); // playback for higher-end PC
+//user_pref("media.memory_cache_max_size",                                  			512000); // memory cache; playback for higher-end PC
+//user_pref("media.memory_caches_combined_limit_kb",                        			2560000); // memory cache; playback for higher-end PC
+user_pref("privacy.sanitize.sanitizeOnShutdown",                            			true);  // clear browsing data on shutdown      //  REMOVE WHEN NOV. UPDATE IS PUSHED
+user_pref("privacy.clearOnShutdown.offlineApps",                            			true);  // disable if login issue after restart; localStorage, service worker cache, QuotaManager (IndexedDB, asm-cache)
+user_pref("dom.security.https_only_fire_http_request_background_timer_ms",  			1000); // decrease timer for HTTP background request
+//user_pref("media.eme.enabled",                                            			false); // disable all DRM content
+//user_pref("browser.eme.ui.enabled",                                       			false); // DRM cotent: hide the setting which also disables the webpage DRM prompt
+user_pref("browser.safebrowsing.provider.google4.gethashURL",               			""); // extra hardening
+user_pref("browser.safebrowsing.provider.google4.updateURL",                			""); // extra hardening
+user_pref("browser.safebrowsing.provider.google.gethashURL",                			""); // extra hardening
+user_pref("browser.safebrowsing.provider.google.updateURL",                 			""); // extra hardening
 
 /** PESKYFOX ***/
 user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites",                   false); // Settings>Home>Firefox Home Content>Recent Activity>Shortcuts>Sponsored shortcuts
@@ -62,11 +62,11 @@ user_pref("ui.key.menuAccessKey",                                               
 
 // Custom monospace font
 // Enforce one custom monospace font (or neither one!) depending on the machine:
-//user_pref("font.name.monospace.x-western",											"SF Mono");			// preferred font [macOS]
-//user_pref("font.name.monospace.x-western",											"Cascadia Mono");	// preferred font [WINDOWS]
+//user_pref("font.name.monospace.x-western",											"SF Mono"); // preferred font [macOS]
+//user_pref("font.name.monospace.x-western",											"Cascadia Mono"); // preferred font [WINDOWS]
 // then uncomment:
 //user_pref("layout.css.font-visibility.trackingprotection",							3); // breaks using custom font on pages
 
 // WINDOWS-only prefs; uncomment to enable:
 //user_pref("gfx.font_rendering.cleartype_params.rendering_mode",						5);	// font improvement [WINDOWS-only]
-//user_pref("gfx.font_rendering.cleartype_params.force_gdi_classic_for_families",		" "); // font improvement [WINDOWS-only]
+//user_pref("gfx.font_rendering.cleartype_params.force_gdi_classic_for_families",		""); // font improvement [WINDOWS-only]
