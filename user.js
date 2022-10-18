24a26edea1989b009dc906dd1ecf8e69bd229e7b
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-10-18 10:19:47 -0400

    pref removed: pdfjs.annotationEditorEnabled", true);

--- user.js
+++ user.js
@@ -218,7 +218,6 @@ user_pref("browser.download.manager.addToRecentDocs", false);
 user_pref("browser.download.always_ask_before_handling_new_types", true);
 
 /** PDF ***/
-user_pref("pdfjs.annotationEditorEnabled", true);
 user_pref("browser.download.open_pdf_attachments_inline", true);
 
 /** TAB BEHAVIOR ***/
