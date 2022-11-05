1a824af69a3894b63ea7e8f781241e57c09ecc9e
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-11-05 12:50:14 -0400

    Update user.js

--- user.js
+++ user.js
@@ -229,7 +229,6 @@ user_pref("browser.download.always_ask_before_handling_new_types", true);
 
 /** PDF ***/
 user_pref("browser.download.open_pdf_attachments_inline", true);
-user_pref("pdfjs.defaultZoomValue", page-width);
 user_pref("pdfjs.sidebarViewOnLoad", 1);
 
 /** TAB BEHAVIOR ***/
