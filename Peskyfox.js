6b13e8bfe941c5cb16f66ed8a926b3cf41cbc2ef
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-11-18 17:48:38 -0500

    Update Peskyfox.js

--- Peskyfox.js
+++ Peskyfox.js
@@ -119,6 +119,10 @@ user_pref("browser.aboutwelcome.enabled", false); // disable Intro screens
 // PREF: show all matches in Findbar
 user_pref("findbar.highlightAll", true);
 
+// PREF: disable middle mouse click opening links from clipboard
+// [1] https://gitlab.torproject.org/tpo/applications/tor-browser/-/issues/10089 ***/
+user_pref("middlemouse.contentLoadURL", false);
+
 // Private Browsing changes [FF106+]
 // PREF: disable private windows being separate from normal windows in taskbar [WINDOWS]
 //user_pref("browser.privateWindowSeparation.enabled", false);
@@ -128,6 +132,25 @@ user_pref("browser.privatebrowsing.enable-new-indicator", false);
 
 // PREF: disable always using dark theme for private browsing windows
 //user_pref("browser.theme.dark-private-windows", false);
+
+// PREF: Cookie Banner handling [NIGHTLY]
+// [NOTE] Feature still enforces Total Cookie Protection to limit 3rd-party cookie tracking
+// [1] https://github.com/mozilla/cookie-banner-rules-list/issues/33#issuecomment-1318460084
+// [2] https://phabricator.services.mozilla.com/D153642
+// 0: Disables all cookie banner handling (default)
+// 1: reject banners if it is a one-click option; otherwise, keep banners on screen
+// 2: reject banners if it is a one-click option; otherwise, fall back to the accept button to remove banner
+user_pref("cookiebanners.service.mode", 1);
+user_pref("cookiebanners.service.mode.privateBrowsing", 1);
+    user_pref("cookiebanners.bannerClicking.enabled", true);
+    //user_pref("cookiebanners.cookieInjector.enabled", true); // DEFAULT
+
+// PREF: enable global CookieBannerRules
+// This is used for click rules that can handle common Consent Management Providers (CMP).
+// [NOTE] Enabling this (when the cookie handling feature is enabled) may
+// negatively impact site performance since it requires us to run rule-defined
+// query selectors for every page.
+//user_pref("cookiebanners.service.enableGlobalRules", true);
 
 /****************************************************************************
  * SECTION: FONT APPEARANCE                                                 *
@@ -494,3 +517,8 @@ user_pref("layout.css.has-selector.enabled", true);
 // too far from the scrollbar, the scrollbar will snap back to the top [LINUX?]
 // default=6
 //user_pref("slider.snapMultiplier", 0);
+
+// PREF: disable websites overriding Firefox's keyboard shortcuts [FF58+]
+// 0 (default) or 1=allow, 2=block
+// [SETTING] to add site exceptions: Ctrl+I>Permissions>Override Keyboard Shortcuts ***/
+//user_pref("permissions.default.shortcuts", 2);
