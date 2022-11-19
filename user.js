bb44f89c32d120383e1a1a22f07c119f4874821c
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-11-18 19:23:05 -0500

    Update user.js

--- user.js
+++ user.js
@@ -118,8 +118,6 @@ user_pref("extensions.postDownloadThirdPartyPrompt", false);
 user_pref("permissions.delegation.enabled", false);
 
 /** HEADERS / REFERERS ***/
-user_pref("network.http.referer.defaultPolicy.trackers", 1);
-user_pref("network.http.referer.defaultPolicy.trackers.pbmode", 1);
 user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
 
 /** CONTAINERS ***/
