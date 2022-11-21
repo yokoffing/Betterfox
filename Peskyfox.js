70472421f8c74c1a366c3fb004f7646bd59371af
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-11-21 10:47:45 -0500

    comment out sidebarviewonload

--- Peskyfox.js
+++ Peskyfox.js
@@ -339,7 +339,7 @@ user_pref("browser.download.open_pdf_attachments_inline", true);
 // 2=table of contents (if not available, will default to 1)
 // 1=view pages
 // -1=disabled (default)
-user_pref("pdfjs.sidebarViewOnLoad", 1);
+//user_pref("pdfjs.sidebarViewOnLoad", 1);
 
 // PREF: default zoom for PDFs [HIDDEN]
 // [NOTE] "page-width" not needed if using sidebar on load
