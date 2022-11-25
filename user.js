c8e32d1a94fc6d94589270d01cdbc8f5401b9934
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-11-25 11:41:08 -0500

    remove media.memory_cache_max_size [now in Fastfox]

--- user.js
+++ user.js
@@ -50,7 +50,6 @@ user_pref("browser.display.use_system_colors", false);
 /** DISK AVOIDANCE ***/
 user_pref("browser.cache.disk.enable", false);
 user_pref("browser.privatebrowsing.forceMediaMemoryCache", true);
-user_pref("media.memory_cache_max_size", 65536);
 user_pref("browser.sessionstore.privacy_level", 2);
 user_pref("browser.helperApps.deleteTempFileOnExit", true);
 user_pref("browser.pagethumbnails.capturing_disabled", true);
