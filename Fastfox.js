ee15269fa0c677c591d748497b5f8130dbe00cf3
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-11-25 11:45:02 -0500

    Update Fastfox.js

--- Fastfox.js
+++ Fastfox.js
@@ -97,29 +97,29 @@ user_pref("layout.css.animation-composition.enabled", true);
  * SECTION: NETWORK                                                         *
 ****************************************************************************/
 
+// PREF: use bigger packets
+// [1] https://www.mail-archive.com/support-seamonkey@lists.mozilla.org/msg74561.html
+// [2] https://www.mail-archive.com/support-seamonkey@lists.mozilla.org/msg74570.html
+user_pref("network.buffer.cache.size", 262144); // preferred=327680; default=32768
+user_pref("network.buffer.cache.count", 128); // preferred=240; default=24
+
+// PREF: DoH requests
+//user_pref("network.trr.request_timeout_ms", 1500); // DEFAULT
+    //user_pref("network.trr.retry-timeout-ms", 125); // DEFAULT
+
+// These do not affect anything, probably:
+
 // PREF: close a connection if tls handshake does not finish in given number of seconds
 //user_pref("network.http.tls-handshake-timeout", 3); // default=30
 
 // PREF: timeout connections if an initial response is not received in number of seconds
 //user_pref("network.http.response.timeout", 5); // default=300
 
-// PREF: use bigger packets
-// [1] https://www.mail-archive.com/support-seamonkey@lists.mozilla.org/msg74561.html
-// [2] https://www.mail-archive.com/support-seamonkey@lists.mozilla.org/msg74570.html
-//user_pref("network.buffer.cache.size", 327680); // alt=262144; default=32768
-//user_pref("network.buffer.cache.count", 240); // alt=128; default=24
-
-// These do not affect anything, probably:
-
 // PREF: increase the absolute number of http connections
 // [1] https://kb.mozillazine.org/Network.http.max-connections
 // [2] https://kb.mozillazine.org/Network.http.max-persistent-connections-per-server
-//user_pref("network.http.max-connections", 2700); // default=900
-    //user_pref("network.http.max-persistent-connections-per-server", 8); // default=6; anything above 10 is excessive
-
-// PREF: DoH requests
-//user_pref("network.trr.request_timeout_ms", 1500); // DEFAULT
-    //user_pref("network.trr.retry-timeout-ms", 125); // DEFAULT
+//user_pref("network.http.max-connections", 1800); // default=900
+    //user_pref("network.http.max-persistent-connections-per-server", 8); // default=6; download connections; anything above 10 is excessive
 
 /****************************************************************************
  * SECTION: MAKE FIREFOX FAST                                               *
@@ -159,8 +159,8 @@ user_pref("gfx.content.skia-font-cache-size", 80);
 
 // PREF: image tweaks
 user_pref("image.cache.size", 10485760);
-user_pref("image.mem.decode_bytes_at_a_time", 65536); // chunk size for calls to the image decoders; alt=262144
-user_pref("image.mem.shared.unmap.min_expiration_ms", 120000); // minimum timeout to unmap shared surfaces since they have been last used
+user_pref("image.mem.decode_bytes_at_a_time", 65536); // alt=262144; chunk size for calls to the image decoders
+user_pref("image.mem.shared.unmap.min_expiration_ms", 120000); // default=60000; minimum timeout to unmap shared surfaces since they have been last used
 user_pref("layers.gpu-process.enabled", true);
     //user_pref("layers.gpu-process.force-enabled", true); // reinforce
 
@@ -173,13 +173,11 @@ user_pref("media.memory_caches_combined_limit_kb", 3145728); // alt=2560000
     //user_pref("media.hardware-video-decoding.force-enabled", true); // reinforce
 
 // PREF: decrease video buffering / increase video preload [not be needed with the above?]
+// [NOTE] Does not affect YouTube since it uses DASH playback [1]
+// [1] https://lifehacker.com/preload-entire-youtube-videos-by-disabling-dash-playbac-1186454034
 //user_pref("media.cache_size", 2048000); // default=512000
 //user_pref("media.cache_readahead_limit", 9000); // default=60; stop reading ahead when our buffered data is this many seconds ahead of the current playback
 //user_pref("media.cache_resume_threshold", 9000); // default=30; when a network connection is suspended, don't resume it until the amount of buffered data falls below this threshold (in seconds)
-// [NOTE] Does not affect YouTube since it uses DASH playback [1]
-// [1] https://lifehacker.com/preload-entire-youtube-videos-by-disabling-dash-playbac-1186454034
-//user_pref("media.mediasource.enabled", false); // disables DASH playback but limits YouTube videos to 720p
-    //user_pref("media.mediasource.experimental.enabled", true); // for testing purposes
     
 // PREF: faster upload speed
 // Firefox currently has a bug with impacting upload speeds with HTTP3/QUIC
