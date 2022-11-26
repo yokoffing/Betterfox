be4ab0d1b90fffa37237bb2a83becd70a19c5128
yokoffing <11689349+yokoffing@users.noreply.github.com>
2022-11-26 12:25:19 -0500

    remove user_pref("pdfjs.sidebarViewOnLoad", 1);

--- user.js
+++ user.js
@@ -10,7 +10,7 @@
 /****************************************************************************
  * Betterfox                                                                *
  * "Ad meliora"                                                             *
- * version: 107a                                                            *
+ * version: 107a1                                                           *
  * url: https://github.com/yokoffing/Betterfox                              *
 ****************************************************************************/
 
@@ -215,7 +215,6 @@ user_pref("browser.download.always_ask_before_handling_new_types", true);
 
 /** PDF ***/
 user_pref("browser.download.open_pdf_attachments_inline", true);
-user_pref("pdfjs.sidebarViewOnLoad", 1);
 
 /** TAB BEHAVIOR ***/
 user_pref("browser.link.open_newwindow.restriction", 0);
