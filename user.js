ad67b611988b98b33576a078e704f0804773dece
yokoffing <11689349+yokoffing@users.noreply.github.com>
2023-01-10 15:26:22 -0500

    move fastfox back to top

--- user.js
+++ user.js
@@ -15,6 +15,48 @@
 ****************************************************************************/
 
 /****************************************************************************
+ * SECTION: FASTFOX                                                         *
+****************************************************************************/
+user_pref("nglayout.initialpaint.delay", 0);
+user_pref("nglayout.initialpaint.delay_in_oopif", 0);
+user_pref("content.notify.interval", 120000); // reset pref; remove in v.110
+user_pref("browser.startup.preXulSkeletonUI", false);
+
+/** EXPERIMENTAL ***/
+user_pref("layout.css.grid-template-masonry-value.enabled", true);
+user_pref("layout.css.animation-composition.enabled", true);
+user_pref("dom.enable_web_task_scheduling", true);
+
+/** GFX ***/
+user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.precache-shaders", true);
+user_pref("gfx.webrender.compositor", true);
+user_pref("layers.gpu-process.enabled", true);
+user_pref("media.hardware-video-decoding.enabled", true);
+user_pref("gfx.canvas.accelerated", true);
+user_pref("gfx.canvas.accelerated.cache-items", 32768);
+user_pref("gfx.canvas.accelerated.cache-size", 4096);
+user_pref("gfx.content.skia-font-cache-size", 80);
+user_pref("image.cache.size", 10485760);
+user_pref("image.mem.decode_bytes_at_a_time", 131072);
+user_pref("image.mem.shared.unmap.min_expiration_ms", 120000);
+user_pref("media.memory_cache_max_size", 1048576);
+user_pref("media.memory_caches_combined_limit_kb", 2560000);
+user_pref("media.cache_readahead_limit", 9000);
+user_pref("media.cache_resume_threshold", 6000);
+
+/** BROWSER CACHE ***/
+user_pref("browser.cache.memory.max_entry_size", 153600);
+
+/** NETWORK ***/
+user_pref("network.buffer.cache.size", 262144);
+user_pref("network.buffer.cache.count", 128);
+user_pref("network.dnsCacheEntries", 20000);	
+user_pref("network.dnsCacheExpiration", 3600);	
+user_pref("network.dnsCacheExpirationGracePeriod", 240);
+user_pref("network.ssl_tokens_cache_capacity", 32768);
+
+/****************************************************************************
  * SECTION: SECUREFOX                                                       *
 ****************************************************************************/
 /** TRACKING PROTECTION ***/
@@ -215,48 +257,6 @@ user_pref("browser.tabs.loadBookmarksInTabs", true);
 user_pref("browser.bookmarks.openInTabClosesMenu", false);
 user_pref("dom.popup_allowed_events", "click dblclick");
 user_pref("layout.css.has-selector.enabled", true);
-
-/****************************************************************************
- * SECTION: FASTFOX                                                         *
-****************************************************************************/
-user_pref("nglayout.initialpaint.delay", 0);
-user_pref("nglayout.initialpaint.delay_in_oopif", 0);
-user_pref("content.notify.interval", 120000); // reset pref; remove in v.110
-user_pref("browser.startup.preXulSkeletonUI", false);
-
-/** EXPERIMENTAL ***/
-user_pref("layout.css.grid-template-masonry-value.enabled", true);
-user_pref("layout.css.animation-composition.enabled", true);
-user_pref("dom.enable_web_task_scheduling", true);
-
-/** GFX ***/
-user_pref("gfx.webrender.all", true);
-user_pref("gfx.webrender.precache-shaders", true);
-user_pref("gfx.webrender.compositor", true);
-user_pref("layers.gpu-process.enabled", true);
-user_pref("media.hardware-video-decoding.enabled", true);
-user_pref("gfx.canvas.accelerated", true);
-user_pref("gfx.canvas.accelerated.cache-items", 32768);
-user_pref("gfx.canvas.accelerated.cache-size", 4096);
-user_pref("gfx.content.skia-font-cache-size", 80);
-user_pref("image.cache.size", 10485760);
-user_pref("image.mem.decode_bytes_at_a_time", 131072);
-user_pref("image.mem.shared.unmap.min_expiration_ms", 120000);
-user_pref("media.memory_cache_max_size", 1048576);
-user_pref("media.memory_caches_combined_limit_kb", 2560000);
-user_pref("media.cache_readahead_limit", 9000);
-user_pref("media.cache_resume_threshold", 6000);
-
-/** BROWSER CACHE ***/
-user_pref("browser.cache.memory.max_entry_size", 153600);
-
-/** NETWORK ***/
-user_pref("network.buffer.cache.size", 262144);
-user_pref("network.buffer.cache.count", 128);
-user_pref("network.dnsCacheEntries", 20000);	
-user_pref("network.dnsCacheExpiration", 3600);	
-user_pref("network.dnsCacheExpirationGracePeriod", 240);
-user_pref("network.ssl_tokens_cache_capacity", 32768);
 
 /****************************************************************************
  * SECTION: SMOOTHFOX                                                       *
