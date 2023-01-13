6653dea918d429044f60ec990cc25ef5b63db349
yokoffing <11689349+yokoffing@users.noreply.github.com>
2023-01-13 10:03:09 -0500

    move accessibility.force_disabled to Secure

--- user.js
+++ user.js
@@ -161,6 +161,7 @@ user_pref("browser.safebrowsing.downloads.remote.enabled", false);
 user_pref("browser.safebrowsing.allowOverride", false);
 
 /** MOZILLA ***/
+user_pref("accessibility.force_disabled", 1);
 user_pref("identity.fxaccounts.enabled", false);
 user_pref("browser.tabs.firefox-view", false);
 user_pref("dom.push.enabled", false);
@@ -210,7 +211,6 @@ user_pref("browser.newtabpage.activity-stream.telemetry", false);
 user_pref("layout.css.prefers-color-scheme.content-override", 2);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("app.update.suppressPrompts", true);
-user_pref("accessibility.force_disabled", 1);
 user_pref("browser.compactmode.show", true);
 user_pref("browser.privatebrowsing.vpnpromourl", "");
 user_pref("extensions.getAddons.showPane", false);
