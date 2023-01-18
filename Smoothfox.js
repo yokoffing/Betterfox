39c19c43ee72c8a06f67198d964056426c169374
yokoffing <11689349+yokoffing@users.noreply.github.com>
2023-01-18 10:39:42 -0500

    Update Smoothfox.js

--- Smoothfox.js
+++ Smoothfox.js
@@ -1,41 +1,32 @@
-//
-/* Do not COPY+PASTE this file. If you do, only COPY+PASTE the user_pref itself.
- *
- * If you make changes to your about:config while the program is running, the
- * changes will be overwritten when the application restarts.
- *
- * To make a change to preferences, you will have to edit the user.js file.
- */
-
 /****************************************************************************************
  * Smoothfox                                                                            *
  * "Faber est suae quisque fortunae"                                                    *
  * priority: better scrolling                                                           *
- * version: November 2022                                                               *
+ * version: January 2023                                                                *
  * url: https://github.com/yokoffing/Betterfox                                          *
  ***************************************************************************************/
 
 /****************************************************************************************
  * OPTION 1: INSTANT SCROLLING (SIMPLE ADJUSTMENT)                                      *
 ****************************************************************************************/
-// recommended for 60hz displays
+// recommended for 60hz+ displays
 user_pref("general.smoothScroll",                       true); // DEFAULT
-user_pref("mousewheel.default.delta_multiplier_y",      275);  // 250-500
+user_pref("mousewheel.default.delta_multiplier_y",      275);  // 250-400
 
 /****************************************************************************************
  * OPTION 2: SMOOTH SCROLLING                                                           *
 ****************************************************************************************/
 // recommended for 90hz+ displays
 user_pref("general.smoothScroll",                       true); // DEFAULT
-user_pref("mousewheel.default.delta_multiplier_y",      265); // 250-500
+user_pref("mousewheel.default.delta_multiplier_y",      300); // 250-400
 user_pref("general.smoothScroll.msdPhysics.enabled",    true);
 
 /****************************************************************************************
- * OPTION 3: NATURAL SMOOTH SCROLLING [MODIFIED]                                        *
+ * OPTION 3: NATURAL SMOOTH SCROLLING V3 [MODIFIED]                                     *
 ****************************************************************************************/
-// from https://github.com/AveYo/fox/blob/main/Natural%20Smooth%20Scrolling%20for%20user.js
-// largely matches Chrome flags: Windows Scrolling Personality and Smooth Scrolling
 // recommended for 120hz+ displays
+// largely matches Chrome flags: Windows Scrolling Personality and Smooth Scrolling
+// from https://github.com/AveYo/fox/blob/cf56d1194f4e5958169f9cf335cd175daa48d349/Natural%20Smooth%20Scrolling%20for%20user.js
 user_pref("general.smoothScroll",                                       true); // DEFAULT
 user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
 user_pref("general.smoothScroll.msdPhysics.enabled",                    true);
@@ -46,5 +37,5 @@ user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",      2.0);
 user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",     250);
 user_pref("general.smoothScroll.currentVelocityWeighting",              1.0);
 user_pref("general.smoothScroll.stopDecelerationWeighting",             1.0);
-user_pref("mousewheel.default.delta_multiplier_y",                      280);
+user_pref("mousewheel.default.delta_multiplier_y",                      300); // 250-400
 
