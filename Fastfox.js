2da06eaa71df37de9ee77fdcc5f514fd8efc40bd
yokoffing <11689349+yokoffing@users.noreply.github.com>
2023-01-25 12:13:03 -0500

    upload speed fix

--- Fastfox.js
+++ Fastfox.js
@@ -254,6 +254,11 @@ user_pref("network.dnsCacheExpirationGracePeriod", 240); // 4 minutes
 // PREF: increase TLS token caching 
 user_pref("network.ssl_tokens_cache_capacity", 32768); // default=2048; more TLS token caching (fast reconnects)
 
+// PREF: temporary fix for upload speed bug in Firefox
+// [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1596576
+user_pref("network.http.http2.send-buffer-size", 33554432);
+user_pref("network.http.http2.push-allowance", 33554432);
+
 /****************************************************************************
  * SECTION: SPECULATIVE CONNECTIONS                                         *
 ****************************************************************************/
