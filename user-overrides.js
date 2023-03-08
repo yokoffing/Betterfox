531a6452db47c2ae24ebdbe62a0976529dbf01a3
H <11689349+yokoffing@users.noreply.github.com>
2023-03-08 12:17:56 -0500

    media.videocontrols.picture-in-picture.improved-video-controls.enabled
    default in v. 112

--- user-overrides.js
+++ user-overrides.js
@@ -92,7 +92,6 @@ user_pref("ui.key.menuAccessKey", 0); // remove underlined characters from vario
 user_pref("general.autoScroll", false); // disable unintentional behavior for middle click
 user_pref("ui.SpellCheckerUnderlineStyle", 1); // dots for spell check errors
 //user_pref("browser.tabs.loadInBackground", false); // CTRL+SHIFT+CLICK for background tabs; Settings>General>Tabs>"When you open a link, image or media in a new tab, switch to it immediately"
-user_pref("media.videocontrols.picture-in-picture.improved-video-controls.enabled", true); // PiP
 user_pref("media.videocontrols.picture-in-picture.display-text-tracks.size", "small"); // PiP
 user_pref("reader.parse-on-load.enabled", false); // disable reader mode
     //user_pref("reader.color_scheme", "auto"); // match system theme for when reader is enabled
