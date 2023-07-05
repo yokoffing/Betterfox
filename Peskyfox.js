bba6d9b40cbfd322a73d9d20b040b18101d8a3fd
HJ <11689349+yokoffing@users.noreply.github.com>
2023-07-05 08:25:13 -0400

    enable Cookie Banner handling

--- Peskyfox.js
+++ Peskyfox.js
@@ -3,7 +3,7 @@
  * Peskyfox                                                                 *
  * "Aquila non capit muscas"                                                *
  * priority: remove annoyances                                              *
- * version: 113                                                             *
+ * version: 115                                                             *
  * url: https://github.com/yokoffing/Betterfox                              *
  ***************************************************************************/
 
@@ -98,27 +98,6 @@ user_pref("browser.privatebrowsing.enable-new-indicator", false);
 
 // PREF: disable always using dark theme for private browsing windows [FF106+]
 //user_pref("browser.theme.dark-private-windows", false);
-
-// PREF: Cookie Banner handling [NIGHTLY]
-// [NOTE] Feature still enforces Total Cookie Protection to limit 3rd-party cookie tracking [1]
-// [1] https://github.com/mozilla/cookie-banner-rules-list/issues/33#issuecomment-1318460084
-// [2] https://phabricator.services.mozilla.com/D153642
-// [3] https://winaero.com/make-firefox-automatically-click-on-reject-all-in-cookie-banner-consent/
-// [4] https://docs.google.com/spreadsheets/d/1Nb4gVlGadyxix4i4FBDnOeT_eJp2Zcv69o-KfHtK-aA/edit#gid=0
-// 2: reject banners if it is a one-click option; otherwise, fall back to the accept button to remove banner
-// 1: reject banners if it is a one-click option; otherwise, keep banners on screen
-// 0: disable all cookie banner handling
-//user_pref("cookiebanners.service.mode", 2);
-//user_pref("cookiebanners.service.mode.privateBrowsing", 2);
-    //user_pref("cookiebanners.bannerClicking.enabled", true); // DEFAULT [FF108]
-    //user_pref("cookiebanners.cookieInjector.enabled", true); // DEFAULT
-
-// PREF: enable global CookieBannerRules
-// This is used for click rules that can handle common Consent Management Providers (CMP)
-// [WARNING] Enabling this (when the cookie handling feature is enabled) may
-// negatively impact site performance since it requires us to run rule-defined
-// query selectors for every page
-//user_pref("cookiebanners.service.enableGlobalRules", enable);
 
 // PREF: Firefox Translations [NIGHTLY ONLY]
 // Visit about:translations to translate your own text as well
@@ -419,6 +398,27 @@ user_pref("browser.bookmarks.openInTabClosesMenu", false);
 //user_pref("dom.popup_allowed_events", "click dblclick");
 //user_pref("dom.disable_open_during_load", true); // DEFAULT
 //user_pref("privacy.popups.showBrowserMessage", true); // DEFAULT
+
+// PREF: Cookie Banner handling [NIGHTLY]
+// [NOTE] Feature still enforces Total Cookie Protection to limit 3rd-party cookie tracking [1]
+// [1] https://github.com/mozilla/cookie-banner-rules-list/issues/33#issuecomment-1318460084
+// [2] https://phabricator.services.mozilla.com/D153642
+// [3] https://winaero.com/make-firefox-automatically-click-on-reject-all-in-cookie-banner-consent/
+// [4] https://docs.google.com/spreadsheets/d/1Nb4gVlGadyxix4i4FBDnOeT_eJp2Zcv69o-KfHtK-aA/edit#gid=0
+// 2: reject banners if it is a one-click option; otherwise, fall back to the accept button to remove banner
+// 1: reject banners if it is a one-click option; otherwise, keep banners on screen
+// 0: disable all cookie banner handling
+user_pref("cookiebanners.service.mode", 2);
+user_pref("cookiebanners.service.mode.privateBrowsing", 2);
+    //user_pref("cookiebanners.bannerClicking.enabled", true); // DEFAULT [FF108]
+    //user_pref("cookiebanners.cookieInjector.enabled", true); // DEFAULT
+
+// PREF: enable global CookieBannerRules
+// This is used for click rules that can handle common Consent Management Providers (CMP)
+// [WARNING] Enabling this (when the cookie handling feature is enabled) may
+// negatively impact site performance since it requires us to run rule-defined
+// query selectors for every page
+//user_pref("cookiebanners.service.enableGlobalRules", enable);
 
 /****************************************************************************
  * SECTION: UNCATEGORIZED                                                   *
