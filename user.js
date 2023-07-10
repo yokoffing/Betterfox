8d238973c408fc64461046c6254835bcfa5e39c9
HJ <11689349+yokoffing@users.noreply.github.com>
2023-07-10 17:58:09 -0400

    re-add DNScacheExpiration

--- user.js
+++ user.js
@@ -55,6 +55,8 @@ user_pref("network.buffer.cache.count", 128);
 user_pref("network.http.max-connections", 1800);
 user_pref("network.http.max-persistent-connections-per-server", 10);
 user_pref("network.dnsCacheEntries", 20000);
+user_pref("network.dnsCacheExpiration", 86400);
+user_pref("network.dnsCacheExpirationGracePeriod", 240);
 user_pref("network.ssl_tokens_cache_capacity", 32768);
 
 /****************************************************************************
