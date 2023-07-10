7e059b3e8a00646893042b10509ccd28f17ddd09
HJ <11689349+yokoffing@users.noreply.github.com>
2023-07-10 13:28:25 -0400

    remove dnsCache entries

--- user-overrides.js
+++ user-overrides.js
@@ -22,9 +22,6 @@ user_pref("browser.sessionstore.restore_pinned_tabs_on_demand", true);
 user_pref("browser.sessionstore.interval", 50000); // 5 min.; set minimum interval between session save operations
 user_pref("media.av1.enabled", false); // disable AV1 to force video hardware decoding
 user_pref("network.http.pacing.requests.min-parallelism", 18); // default=6
-user_pref("network.dnsCacheEntries", 20000); // maximum # of DNS entries
-//user_pref("network.dnsCacheExpiration", 86400); // keep DNS entries for 24 hours
-//user_pref("network.dnsCacheExpirationGracePeriod", 240); // 4 minutes
 user_pref("network.http.speculative-parallel-limit", 18); // default=6
 user_pref("network.dns.disablePrefetch", false);
     user_pref("network.dns.disablePrefetchFromHTTPS", false);
