e49c7b92d742723420099f1fe88696248d3b91db
HJ <11689349+yokoffing@users.noreply.github.com>
2023-07-24 10:13:39 -0700

    remove beta format spacing

--- Smoothfox.js
+++ Smoothfox.js
@@ -12,16 +12,16 @@
 // recommended for 60hz+ displays
 user_pref("apz.overscroll.enabled", true); // not DEFAULT on Linux
 user_pref("general.smoothScroll", true); // DEFAULT
-user_pref("mousewheel.default.delta_multiplier_y", 275); // 250-400
+user_pref("mousewheel.default.delta_multiplier_y", 275); // 250-400; adjust this number to your liking
 
 /****************************************************************************************
  * OPTION 2: SMOOTH SCROLLING                                                           *
 ****************************************************************************************/
 // recommended for 90hz+ displays
 user_pref("apz.overscroll.enabled", true); // not DEFAULT on Linux
-user_pref("general.smoothScroll",                       true); // DEFAULT
-user_pref("mousewheel.default.delta_multiplier_y",      300); // 250-400
-user_pref("general.smoothScroll.msdPhysics.enabled",    true);
+user_pref("general.smoothScroll", true); // DEFAULT
+user_pref("mousewheel.default.delta_multiplier_y", 300); // 250-400; adjust this number to your liking
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
 
 /****************************************************************************************
  * OPTION 3: NATURAL SMOOTH SCROLLING V3 [MODIFIED]                                     *
@@ -30,15 +30,14 @@ user_pref("general.smoothScroll.msdPhysics.enabled",    true);
 // largely matches Chrome flags: Windows Scrolling Personality and Smooth Scrolling
 // from https://github.com/AveYo/fox/blob/cf56d1194f4e5958169f9cf335cd175daa48d349/Natural%20Smooth%20Scrolling%20for%20user.js
 user_pref("apz.overscroll.enabled", true); // not DEFAULT on Linux
-user_pref("general.smoothScroll",                                       true); // DEFAULT
+user_pref("general.smoothScroll", true); // DEFAULT
 user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
-user_pref("general.smoothScroll.msdPhysics.enabled",                    true);
-user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",  600);
-user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",      650);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",         25);
-user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",      2.0);
-user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",     250);
-user_pref("general.smoothScroll.currentVelocityWeighting",              1.0);
-user_pref("general.smoothScroll.stopDecelerationWeighting",             1.0);
-user_pref("mousewheel.default.delta_multiplier_y",                      300); // 250-400
-
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 650);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 25);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", 2.0);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 250);
+user_pref("general.smoothScroll.currentVelocityWeighting", 1.0);
+user_pref("general.smoothScroll.stopDecelerationWeighting", 1.0);
+user_pref("mousewheel.default.delta_multiplier_y", 300); // 250-400; adjust this number to your liking
