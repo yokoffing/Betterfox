db658e89bd21ab66697f38ae33bf760ab5cf9af9
HJ <11689349+yokoffing@users.noreply.github.com>
2023-08-01 11:01:31 -0400

    Update Fastfox.js

--- Fastfox.js
+++ Fastfox.js
@@ -305,12 +305,12 @@ user_pref("network.ssl_tokens_cache_capacity", 32768); // default=2048; more TLS
 // [NOTE] Add prefs to "MY OVERRIDES" section and uncomment to enable them in your user.js.
 
 // PREF: increase network predictions
-//user_pref("network.http.speculative-parallel-limit", 18); // default=6; overrides SecureFox
+//user_pref("network.http.speculative-parallel-limit", 12); // default=6; overrides SecureFox
 //user_pref("network.dns.disablePrefetch", false); // overrides SecureFox
 //user_pref("network.dns.disablePrefetchFromHTTPS", false);
 //user_pref("network.early-hints.enabled", true);
     //user_pref("network.early-hints.preconnect.enabled", true);
-    //user_pref("network.early-hints.preconnect.max_connections", 20); // FF113
+    //user_pref("network.early-hints.preconnect.max_connections", 18); // default=10
 //user_pref("browser.urlbar.speculativeConnect.enabled", true); // overrides SecureFox
 //user_pref("browser.places.interactions.enabled", true); // DEFAULT
     //user_pref("browser.places.speculativeConnect.enabled", true); // overrides SecureFox
@@ -318,10 +318,11 @@ user_pref("network.ssl_tokens_cache_capacity", 32768); // default=2048; more TLS
 //user_pref("network.predictor.enabled", true); // overrides SecureFox
 //user_pref("network.predictor.enable-prefetch", true); // overrides SecureFox
 //user_pref("network.predictor.enable-hover-on-ssl", true);
-    //user_pref("network.predictor.preresolve-min-confidence", 40); // default=60; alt=10
+    //user_pref("network.predictor.preresolve-min-confidence", 30); // default=60; alt=10
     //user_pref("network.predictor.preconnect-min-confidence", 60); // default=90; alt=20
-    //user_pref("network.predictor.prefetch-min-confidence", 80); // default=100; alt=30
+    //user_pref("network.predictor.prefetch-min-confidence", 90); // default=100; alt=30
         //user_pref("network.predictor.prefetch-force-valid-for", 3600); // default=10
         //user_pref("network.predictor.prefetch-rolling-load-count", 120); // default=10
     //user_pref("network.predictor.max-resources-per-entry", 250); // default=100
     //user_pref("network.predictor.max-uri-length", 1000); // default=500
+
