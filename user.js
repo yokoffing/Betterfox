7b1c5637a7baee4c4c906f04aec040b38b358fe8
HJ <11689349+yokoffing@users.noreply.github.com>
2023-08-03 22:36:15 -0400

    Update user.js

--- user.js
+++ user.js
@@ -41,8 +41,8 @@ user_pref("browser.cache.memory.capacity", 1048576);
 user_pref("browser.cache.memory.max_entry_size", 65536);
 
 /** MEDIA CACHE ***/
-user_pref("media.cache_readahead_limit", 900);
-user_pref("media.cache_resume_threshold", 480);
+user_pref("media.cache_readahead_limit", 7200);
+user_pref("media.cache_resume_threshold", 3600);
 user_pref("media.memory_cache_max_size", 131072);
 user_pref("media.memory_caches_combined_limit_kb", 524288); // DEFAULT
 
