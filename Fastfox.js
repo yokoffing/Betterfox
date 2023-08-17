254f1df1140f65f813b797afa03bd926bc92019c
HJ <11689349+yokoffing@users.noreply.github.com>
2023-08-17 11:57:23 -0400

    add link

--- Fastfox.js
+++ Fastfox.js
@@ -306,9 +306,13 @@ user_pref("browser.cache.disk.enable", false);
 //user_pref("browser.cache.frecency_half_life_hours", 24); // default=6
 
 // PREF: memory cache
-// The "automatic" size selection (default) is based on a decade-old table that only contains settings for systems at or below 8GB of system memory [1]
-// -1=Automatically decide the maximum memory to use to cache decoded images, messages, and chrome based on the total amount of RAM
+// The "automatic" size selection (default) is based on a decade-old table
+// that only contains settings for systems at or below 8GB of system memory [1].
+// Value can be up to the max size of an unsigned 64-bit integer.
+// -1=Automatically decide the maximum memory to use to cache decoded images,
+// messages, and chrome based on the total amount of RAM
 // [1] https://kb.mozillazine.org/Browser.cache.memory.capacity#-1
+// [2] https://searchfox.org/mozilla-central/source/netwerk/cache2/CacheObserver.cpp#94-125
 user_pref("browser.cache.memory.capacity", 1048576); // default=-1; 1048576=1GB, 2097152=2GB
 user_pref("browser.cache.memory.max_entry_size", 65536); // default=5120; -1=entries bigger than than 90% of the mem-cache are never cached
 
