9c982727606d84403926fd6968ed133c553bd506
HJ <11689349+yokoffing@users.noreply.github.com>
2023-08-27 15:40:40 -0400

    Update Peskyfox.js

--- Peskyfox.js
+++ Peskyfox.js
@@ -13,8 +13,8 @@
 
 // PREF: preferred color scheme for websites
 // [SETTING] General>Language and Appearance>Website appearance
-// By default, it matches the theme of your toolbar (3).
-// Set this pref to choose dark theme on sites that support it (0) or to a light theme (1).
+// By default, color scheme matches the theme of your browser toolbar (3).
+// Set this pref to choose Dark on sites that support it (0) or Light (1).
 // Before FF95, the pref was 2, which determined site color based on OS theme.
 // Dark (0), Light (1), System (2), Browser (3) (default [FF95+])
 // [1] https://www.reddit.com/r/firefox/comments/rfj6yc/how_to_stop_firefoxs_dark_theme_from_overriding/hoe82i5/?context=3
