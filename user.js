db40af77f1b224d23742dd4608a194aa9a3e7d79
HJ <11689349+yokoffing@users.noreply.github.com>
2023-08-28 10:11:01 -0400

    reorder

--- user.js
+++ user.js
@@ -227,9 +227,9 @@ user_pref("extensions.pocket.enabled", false);
 
 /** DOWNLOADS ***/
 user_pref("browser.download.useDownloadDir", false);
+user_pref("browser.download.always_ask_before_handling_new_types", true);
 user_pref("browser.download.alwaysOpenPanel", false);
 user_pref("browser.download.manager.addToRecentDocs", false);
-user_pref("browser.download.always_ask_before_handling_new_types", true);
 
 /** PDF ***/
 user_pref("browser.download.open_pdf_attachments_inline", true);
