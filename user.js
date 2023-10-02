d941d2b8c715c1fe9e59799c88394144a70cb4c3
HJ <11689349+yokoffing@users.noreply.github.com>
2023-10-02 11:39:12 -0400

    remove browser.urlbar.suggest.engines

--- user.js
+++ user.js
@@ -202,7 +202,6 @@ user_pref("full-screen-api.warning.delay", -1);
 user_pref("full-screen-api.warning.timeout", 0);
 
 /** URL BAR ***/
-user_pref("browser.urlbar.suggest.engines", false);
 user_pref("browser.urlbar.suggest.calculator", true);
 user_pref("browser.urlbar.unitConversion.enabled", true);
 
