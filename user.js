70b7196e5d8b431778c9f8ed72f6a73468d5dd8e
HJ <11689349+yokoffing@users.noreply.github.com>
2023-10-03 13:52:22 -0400

    remove suppress prompts

--- user.js
+++ user.js
@@ -175,7 +175,6 @@ user_pref("network.connectivity-service.enabled", false);
 /** MOZILLA UI ***/
 user_pref("layout.css.prefers-color-scheme.content-override", 2);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("app.update.suppressPrompts", true);
 user_pref("browser.compactmode.show", true);
 user_pref("browser.privatebrowsing.vpnpromourl", "");
 user_pref("extensions.getAddons.showPane", false);
