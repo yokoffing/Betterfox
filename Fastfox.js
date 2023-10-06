5755c0d7bc89a5d8c4e17a339926c8856b7ee54c
HJ <11689349+yokoffing@users.noreply.github.com>
2023-10-05 23:05:28 -0400

    Update Fastfox.js

--- Fastfox.js
+++ Fastfox.js
@@ -376,12 +376,12 @@ user_pref("network.http.pacing.requests.enabled", false);
     //user_pref("network.http.pacing.requests.burst", 14); // default=10
 
 // PREF: how long to wait before trying a different connection when the initial one fails
-// The number (in ms) after sending a SYN for an HTTP connection,
-// to wait before trying again with a different connection.
+// This value signifies how long (in ms) Firefox waits after dispatching
+// a SYN for an HTTP connection before attempting a different connection.
 // 0=disable the second connection
 // [1] https://searchfox.org/mozilla-esr115/source/modules/libpref/init/all.js#1178
 // [2] https://www.catchpoint.com/blog/http-transaction-steps
-//user_pref("network.http.connection-retry-timeout", 0); // default=250
+user_pref("network.http.connection-retry-timeout", 0); // default=250
 
 // PREF: increase DNS cache
 // [1] https://developer.mozilla.org/en-US/docs/Web/Performance/Understanding_latency
