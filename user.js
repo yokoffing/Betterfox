9fd6cbddb706c6c4417f972fc202b3e5073593d0
HJ <11689349+yokoffing@users.noreply.github.com>
2023-10-05 23:05:43 -0400

    Update user.js

--- user.js
+++ user.js
@@ -48,6 +48,7 @@ user_pref("network.http.max-connections", 1800);
 user_pref("network.http.max-persistent-connections-per-server", 10);
 user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
 user_pref("network.http.pacing.requests.enabled", false);
+user_pref("network.http.connection-retry-timeout", 0);
 user_pref("network.dnsCacheExpiration", 86400);
 user_pref("network.dns.max_high_priority_threads", 8);
 user_pref("network.ssl_tokens_cache_capacity", 10240);
