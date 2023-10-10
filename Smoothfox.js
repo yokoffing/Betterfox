0c9bed985554d3a0b9a31247a9918212e4d876d1
yokoffing <11689349+yokoffing@users.noreply.github.com>
2023-10-10 13:25:50 -0400

    add note

--- Smoothfox.js
+++ Smoothfox.js
@@ -14,7 +14,7 @@
 ****************************************************************************************/
 // credit: https://github.com/black7375/Firefox-UI-Fix
 // only sharpen scrolling
-user_pref("mousewheel.min_line_scroll_amount", 10); // 10-40; default=5
+user_pref("mousewheel.min_line_scroll_amount", 20); // 10-40; adjust this number to your liking; default=5
 user_pref("general.smoothScroll.mouseWheel.durationMinMS", 80); // default=50
 user_pref("general.smoothScroll.currentVelocityWeighting", "0.15"); // default=.25
 user_pref("general.smoothScroll.stopDecelerationWeighting", "0.6"); // default=.4
