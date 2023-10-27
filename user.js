da0fcffe71644193c5916de1bf61c19ea2404d49
yokoffing <11689349+yokoffing@users.noreply.github.com>
2023-10-26 23:11:55 -0400

    remove network.predictor.enable-prefetch

--- user.js
+++ user.js
@@ -51,7 +51,6 @@ user_pref("network.ssl_tokens_cache_capacity", 10240);
 user_pref("network.dns.disablePrefetch", true);
 user_pref("network.prefetch-next", false);
 user_pref("network.predictor.enabled", false);
-user_pref("network.predictor.enable-prefetch", false);
 
 /** EXPERIMENTAL ***/
 user_pref("layout.css.grid-template-masonry-value.enabled", true);
