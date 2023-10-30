a48f7002827e0f60b4f4a8489611c1f7de596ab5
yokoffing <11689349+yokoffing@users.noreply.github.com>
2023-10-30 09:19:32 -0400

    add layout.word_select.eat_space_to_next_word

--- user.js
+++ user.js
@@ -235,6 +235,7 @@ user_pref("browser.download.open_pdf_attachments_inline", true);
 user_pref("browser.bookmarks.openInTabClosesMenu", false);
 user_pref("browser.menu.showViewImageInfo", true);
 user_pref("findbar.highlightAll", true);
+user_pref("layout.word_select.eat_space_to_next_word", false);
 
 /****************************************************************************
  * START: MY OVERRIDES                                                      *
