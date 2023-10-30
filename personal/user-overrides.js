2f3bbf5747370961c2ad25d1c19fa7551cd5f0d2
yokoffing <11689349+yokoffing@users.noreply.github.com>
2023-10-30 09:20:21 -0400

    Update user-overrides.js

--- personal/user-overrides.js
+++ personal/user-overrides.js
@@ -86,7 +86,7 @@ user_pref("view_source.wrap_long_lines", true);  // wrap source lines
 user_pref("devtools.debugger.ui.editor-wrapping", true);  // wrap lines in devtools
 user_pref("browser.zoom.full", false); // text-only zoom, not all elements on page
 user_pref("pdfjs.sidebarViewOnLoad", 2); // force showing of Table of Contents in sidebar for PDFs (if available)
-user_pref("layout.word_select.eat_space_to_next_word", false); // do not select the space next to a word when selecting a word
+//user_pref("layout.word_select.eat_space_to_next_word", false); // do not select the space next to a word when selecting a word
 //user_pref("browser.tabs.loadInBackground", false); // CTRL+SHIFT+CLICK for background tabs; Settings>General>Tabs>When you open a link, image or media in a new tab, switch to it immediately
 user_pref("browser.tabs.loadBookmarksInTabs", true); // force bookmarks to open in a new tab, not the current tab
 user_pref("ui.key.menuAccessKey", 0); // remove underlined characters from various settings
