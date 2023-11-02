04acc403c5bbba62143311f65f458b759dd0f2be
yokoffing <11689349+yokoffing@users.noreply.github.com>
2023-11-02 19:35:43 -0400

    add security.mixed_content.upgrade_display_content.image

--- user.js
+++ user.js
@@ -122,6 +122,7 @@ user_pref("extensions.formautofill.creditCards.enabled", false);
 /** MIXED CONTENT + CROSS-SITE ***/
 user_pref("security.mixed_content.block_display_content", true);
 user_pref("security.mixed_content.upgrade_display_content", true);
+user_pref("security.mixed_content.upgrade_display_content.image", true);
 user_pref("pdfjs.enableScripting", false);
 user_pref("extensions.postDownloadThirdPartyPrompt", false);
 
