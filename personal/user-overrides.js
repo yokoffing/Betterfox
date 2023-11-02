8ea3a00d1e18c234e08471883afdd275815ca42b
yokoffing <11689349+yokoffing@users.noreply.github.com>
2023-11-02 11:54:44 -0400

    Update user-overrides.js

--- personal/user-overrides.js
+++ personal/user-overrides.js
@@ -22,6 +22,18 @@ user_pref("browser.sessionstore.restore_pinned_tabs_on_demand", true);
 user_pref("browser.sessionhistory.max_total_viewers", 4); // only remember # of pages in Back-Forward cache
 user_pref("network.fetchpriority.enabled", true); // fetchPriority property
 
+/** cache test ***/
+user_pref("browser.cache.disk.enable", true);
+user_pref("browser.cache.disk.smart_size.enabled", false);
+user_pref("browser.cache.disk.capacity", 512000);
+user_pref("browser.cache.disk.metadata_memory_limit", 500); 
+user_pref("browser.cache.disk.preload_chunk_count", 8);
+user_pref("browser.cache.jsbc_compression_level", 3);
+user_pref("browser.cache.memory.capacity", 256000); // default= -1 (32768)
+user_pref("browser.cache.memory.max_entry_size", 10240); // default=5120 (5 MB)
+user_pref("media.memory_caches_combined_limit_kb", 1048576); // default=524288
+user_pref("media.memory_caches_combined_limit_pc_sysmem", 10); // default=5
+
 /** speculative load test ***/
 user_pref("network.http.speculative-parallel-limit", 10);
 user_pref("network.dns.disablePrefetchFromHTTPS", false);
@@ -97,12 +109,12 @@ user_pref("media.videocontrols.picture-in-picture.urlbar-button.enabled", false)
 user_pref("reader.parse-on-load.enabled", false); // disable reader mode
 //user_pref("reader.color_scheme", "auto"); // match system theme for when reader is enabled
 //user_pref("browser.urlbar.openintab", true); // stay on current site and open new tab when typing in URL bar
+user_pref("browser.urlbar.trimHttps", true); // hide https in URL bar [FF119]
 
 /** DELETE IF NOT NIGHTLY ***/
 user_pref("privacy.userContext.enabled", false); // disable Containers functionality
 user_pref("browser.crashReports.unsubmittedCheck.enabled", false); // true by default on NIGHTLY
 //user_pref("xpinstall.signatures.required", false); // [ESR/DEV/NIGHTLY]
-user_pref("browser.urlbar.trimHttps", true); // hide https in URL bar [FF119]
 
 /** DELETE IF NOT WINDOWS DESKTOP ***/
 user_pref("network.trr.mode", 3); // enable TRR (without System fallback)
@@ -120,8 +132,6 @@ user_pref("gfx.font_rendering.directwrite.use_gdi_table_loading", false);
 //user_pref("font.name.serif.x-western", "Roboto Slab"); // serif font
 //user_pref("font.name.sans-serif.x-western", "Roboto"); // sans-serif font
 //user_pref("font.name.monospace.x-western", "Fira Code"); // monospace font
-user_pref("browser.cache.disk.capacity", 1024000);
-user_pref("browser.cache.disk.metadata_memory_limit", 1000);
 
 /** DELETE IF NOT macOS LAPTOP ***/
 user_pref("network.trr.mode", 2); // enable TRR (with System fallback)
