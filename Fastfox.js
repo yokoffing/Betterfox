efc94334b9f4320b12af0b56017be081374dc547
yokoffing <11689349+yokoffing@users.noreply.github.com>
2023-11-02 11:58:40 -0400

    remove network.http.connection-retry-timeout
    causes issues

--- Fastfox.js
+++ Fastfox.js
@@ -334,14 +334,6 @@ user_pref("network.http.pacing.requests.enabled", false);
     //user_pref("network.http.pacing.requests.min-parallelism", 10); // default=6
     //user_pref("network.http.pacing.requests.burst", 14); // default=10
 
-// PREF: how long to wait before trying a different connection when the initial one fails
-// This value (in milliseconds) specifies the delay after sending a SYN
-// for an HTTP connection before proceeding with a different connection.
-// 0=disables the second connection
-// [1] https://searchfox.org/mozilla-esr115/source/modules/libpref/init/all.js#1178
-// [2] https://www.catchpoint.com/blog/http-transaction-steps
-//user_pref("network.http.connection-retry-timeout", 0); // default=250
-
 // PREF: increase DNS cache
 // [1] https://developer.mozilla.org/en-US/docs/Web/Performance/Understanding_latency
 //user_pref("network.dnsCacheEntries", 1000); // default=400
