8fce1e91f75afa1ff1fef66b13b19236fee2fbdb
Hal <11689349+yokoffing@users.noreply.github.com>
2024-01-08 10:19:12 -0500

    Update Smoothfox.js

--- Smoothfox.js
+++ Smoothfox.js
@@ -8,6 +8,8 @@
  ***************************************************************************************/
 
 // Note: msdPhysics was enabled by default for 122 Nightly.
+// The options below have not been modified to account for this change.
+// [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1846935
 
 // Use only one option at a time!
 // Reset prefs if you decide to use different option.
