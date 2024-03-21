c18c4802f407b3235a73450dbf990be51a6ee996
Hal <11689349+yokoffing@users.noreply.github.com>
2024-03-21 09:41:23 -0400

    layout.css.has-selector.enabled is now default

--- user.js
+++ user.js
@@ -52,7 +52,6 @@ user_pref("network.predictor.enabled", false);
 /** EXPERIMENTAL ***/
 user_pref("layout.css.grid-template-masonry-value.enabled", true);
 user_pref("dom.enable_web_task_scheduling", true);
-user_pref("layout.css.has-selector.enabled", true);
 user_pref("dom.security.sanitizer.enabled", true);
 
 /****************************************************************************
