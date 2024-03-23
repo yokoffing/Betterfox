576f0607d4ea6ed961e91284b6717f5076c26347
Hal <11689349+yokoffing@users.noreply.github.com>
2024-03-23 13:34:40 -0400

    remove network.buffer.cache
    https://github.com/yokoffing/Betterfox/issues/279#issuecomment-1991864898

--- user.js
+++ user.js
@@ -10,7 +10,7 @@
 /****************************************************************************
  * Betterfox                                                                *
  * "Ad meliora"                                                             *
- * version: esr115.4                                                        *
+ * version: esr115.5                                                        *
  * url: https://github.com/yokoffing/Betterfox                              *
 ****************************************************************************/
 
@@ -41,8 +41,6 @@ user_pref("media.cache_resume_threshold", 3600);
 user_pref("image.mem.decode_bytes_at_a_time", 32768);
 
 /** NETWORK ***/
-user_pref("network.buffer.cache.size", 262144);
-user_pref("network.buffer.cache.count", 128);
 user_pref("network.http.max-connections", 1800);
 user_pref("network.http.max-persistent-connections-per-server", 10);
 user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
