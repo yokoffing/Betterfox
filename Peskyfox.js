36f63d97dc7d007008ae7f0159429a0afb1b6099
Hal <11689349+yokoffing@users.noreply.github.com>
2024-04-16 10:11:53 -0400

    browser.urlbar.yelp.featureGate

--- Peskyfox.js
+++ Peskyfox.js
@@ -217,6 +217,7 @@ user_pref("browser.urlbar.trending.featureGate", false);
 //user_pref("browser.urlbar.pocket.featureGate", false); // [FF116+] [DEFAULT: false]
 //user_pref("browser.urlbar.weather.featureGate", false); // [FF108+] [DEFAULT: false]
 //user_pref("browser.urlbar.clipboard.featureGate", false); // [FF118+] [DEFAULT: true FF125+]
+//user_pref("browser.urlbar.yelp.featureGate", false); // [FF123+] [DEFAULT: false]
 
 // PREF: disable tab-to-search [FF85+]
 // Alternatively, you can exclude on a per-engine basis by unchecking them in Options>Search
