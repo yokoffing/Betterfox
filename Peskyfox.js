e9b0b4f225d7384bb205e2a90eeaee5f3448c9cd
Hal <11689349+yokoffing@users.noreply.github.com>
2024-05-20 13:13:08 -0400

    ver #

--- Peskyfox.js
+++ Peskyfox.js
@@ -3,7 +3,7 @@
  * Peskyfox                                                                 *
  * "Aquila non capit muscas"                                                *
  * priority: remove annoyances                                              *
- * version: 122                                                             *
+ * version: 126                                                             *
  * url: https://github.com/yokoffing/Betterfox                              *
  * credit: Some prefs are reproduced and adapted from the arkenfox project  *
  * credit urL: https://github.com/arkenfox/user.js                          *
