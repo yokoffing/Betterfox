23d54af0bb7801643d285968cd3d0b1d5a9628fa
yokoffing <11689349+yokoffing@users.noreply.github.com>
2024-06-23 13:05:11 -0400

    enable browser.urlbar.untrimOnUserInteraction.featureGate

--- Securefox.js
+++ Securefox.js
@@ -529,8 +529,9 @@ user_pref("privacy.history.custom", true);
  * SECTION: SEARCH / URL BAR                                                 *
 ******************************************************************************/
 
-// PREF: darken certain parts of the URL
+// PREF: darken certain parts of the URL [FF75+]
 // Makes the domain name more prominent by graying out other parts of the URL.
+// Also hidse https:// and www parts from the suggestion URL.
 // [1] https://udn.realityripple.com/docs/Mozilla/Preferences/Preference_reference/browser.urlbar.trimURLs
 // [2] https://winaero.com/firefox-75-strips-https-and-www-from-address-bar-results/
 //user_pref("browser.urlbar.trimURLs", true); // DEFAULT
@@ -542,10 +543,10 @@ user_pref("privacy.history.custom", true);
 // so it's not like we need to see https. It's not a privacy issue, so you can add to your overrides.
 // [TEST] http://www.http2demo.io/
 // [1] https://www.ghacks.net/2023/09/19/firefox-119-will-launch-with-an-important-address-bar-change/
-//user_pref("browser.urlbar.trimHttps", true);
+user_pref("browser.urlbar.trimHttps", true);
 
 // PREF: reveal HTTPS in the URL upon double click [FF127+]
-//user_pref("browser.urlbar.untrimOnUserInteraction.featureGate", true);
+user_pref("browser.urlbar.untrimOnUserInteraction.featureGate", true);
 
 // PREF: display "Not Secure" text on HTTP sites
 // Needed with HTTPS-First Policy; not needed with HTTPS-Only Mode.
