a0678f3859348d6efa287a14535c947fd0360291
yokoffing <11689349+yokoffing@users.noreply.github.com>
2024-08-17 10:48:38 -0400

    remove download pref for noobs

--- user.js
+++ user.js
@@ -215,7 +215,6 @@ user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
 user_pref("extensions.pocket.enabled", false);
 
 /** DOWNLOADS ***/
-user_pref("browser.download.always_ask_before_handling_new_types", true);
 user_pref("browser.download.manager.addToRecentDocs", false);
 
 /** PDF ***/
