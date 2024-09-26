5a67c4f5e99bbfca68de9ef3a998801f378c2187
yokoffing <11689349+yokoffing@users.noreply.github.com>
2024-09-26 14:35:14 -0400

    css grid

--- user.js
+++ user.js
@@ -52,7 +52,6 @@ user_pref("network.predictor.enabled", false);
 user_pref("network.predictor.enable-prefetch", false);
 
 /** EXPERIMENTAL ***/
-user_pref("layout.css.grid-template-masonry-value.enabled", true);
 user_pref("dom.enable_web_task_scheduling", true);
 
 /****************************************************************************
