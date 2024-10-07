83b1874de03b643aed4b01e8df77bf292be7a845
yokoffing <11689349+yokoffing@users.noreply.github.com>
2024-10-07 07:12:44 -0400

    Update user.js

--- user.js
+++ user.js
@@ -53,6 +53,7 @@ user_pref("network.predictor.enable-prefetch", false);
 
 /** EXPERIMENTAL ***/
 user_pref("dom.enable_web_task_scheduling", true);
+user_pref("layout.css.grid-template-masonry-value.enabled", true);
 
 /****************************************************************************
  * SECTION: SECUREFOX                                                       *
