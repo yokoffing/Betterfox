cb297c695b87ba32d2593d16eaa3f278fe2b9299
yokoffing <11689349+yokoffing@users.noreply.github.com>
2025-03-24 08:48:00 -0400

    browser.urlbar.suggest.calculator default

--- user.js
+++ user.js
@@ -179,7 +179,6 @@ user_pref("full-screen-api.transition-duration.leave", "0 0");
 user_pref("full-screen-api.warning.timeout", 0);
 
 /** URL BAR ***/
-user_pref("browser.urlbar.suggest.calculator", true);
 user_pref("browser.urlbar.unitConversion.enabled", true);
 user_pref("browser.urlbar.trending.featureGate", false);
 
