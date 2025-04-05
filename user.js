aff7b731d2c427ea7edcff20617b07362d2dae59
yokoffing <11689349+yokoffing@users.noreply.github.com>
2025-04-05 15:00:16 -0400

    Update user.js

--- user.js
+++ user.js
@@ -36,8 +36,6 @@ user_pref("media.cache_resume_threshold", 3600);
 user_pref("image.mem.decode_bytes_at_a_time", 32768);
 
 /** NETWORK ***/
-user_pref("network.buffer.cache.size", 65535);
-user_pref("network.buffer.cache.count", 48);
 user_pref("network.http.max-connections", 1800);
 user_pref("network.http.max-persistent-connections-per-server", 10);
 user_pref("network.http.max-urgent-start-excessive-connections-per-host", 5);
