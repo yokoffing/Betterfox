6f14571c97a73a7a5dfa9b8968e70a00a65e0625
yokoffing <11689349+yokoffing@users.noreply.github.com>
2025-10-06 16:53:37 -0400

    remove content.notify.interval for now

--- user.js
+++ user.js
@@ -17,9 +17,6 @@
 /****************************************************************************
  * SECTION: FASTFOX                                                         *
 ****************************************************************************/
-/** GENERAL ***/
-user_pref("content.notify.interval", 100000);
-
 /** GFX ***/
 user_pref("gfx.canvas.accelerated.cache-size", 512);
 user_pref("gfx.content.skia-font-cache-size", 20);
@@ -227,6 +224,7 @@ user_pref("layout.word_select.eat_space_to_next_word", false);
 /****************************************************************************
  * END: BETTERFOX                                                           *
 ****************************************************************************/
+
 
 
 
