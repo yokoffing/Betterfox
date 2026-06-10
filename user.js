a7818d1008dbdb8750eb2b0e85a1875cae340530
yokoffing <11689349+yokoffing@users.noreply.github.com>
2026-06-09 20:11:18 -0400

    remove view image and eat space

--- user.js
+++ user.js
@@ -173,9 +173,7 @@ user_pref("browser.download.open_pdf_attachments_inline", true);
 
 /** TAB BEHAVIOR ***/
 user_pref("browser.bookmarks.openInTabClosesMenu", false);
-user_pref("browser.menu.showViewImageInfo", true);
 user_pref("findbar.highlightAll", true);
-user_pref("layout.word_select.eat_space_to_next_word", false);
 
 /****************************************************************************
  * SECTION: SMOOTHFOX                                                       *
