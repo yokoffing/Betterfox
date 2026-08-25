9da244e44185c99ff036b15c0fc27f66ef63f720
yokoffing <11689349+yokoffing@users.noreply.github.com>
2026-08-25 18:40:33 -0400

    disable remote improvements

--- user.js
+++ user.js
@@ -142,6 +142,7 @@ user_pref("datareporting.usage.uploadEnabled", false);
 user_pref("app.shield.optoutstudies.enabled", false);
 user_pref("app.normandy.enabled", false);
 user_pref("app.normandy.api_url", "");
+user_pref("nimbus.rollouts.enabled", false);
 
 /** CRASH REPORTS ***/
 user_pref("breakpad.reportURL", "");
