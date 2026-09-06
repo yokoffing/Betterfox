94eeb5bf4c1dd8c2d4b20fe2fd9d1474713d2b69
yokoffing <11689349+yokoffing@users.noreply.github.com>
2026-09-06 12:41:31 -0400

    fix ver num

--- Securefox.js
+++ Securefox.js
@@ -1194,8 +1194,8 @@ user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
 // [SETTING] General>Tabs>Enable Container Tabs
 // [1] https://wiki.mozilla.org/Security/Contextual_Identity_Project/Containers
 // [2] https://addons.mozilla.org/en-US/firefox/addon/multi-account-containers/
-//user_pref("privacy.userContext.ui.enabled", true); [DEFAULT FF154+]
-//user_pref("privacy.userContext.enabled", true); [DEFAULT FF154+]
+//user_pref("privacy.userContext.ui.enabled", true); [DEFAULT FF153+]
+//user_pref("privacy.userContext.enabled", true); [DEFAULT FF153+]
 
 // PREF: set behavior on "+ Tab" button to display container menu on left click [FF74+]
 // [NOTE] The menu is always shown on long press and right click.
