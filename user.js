792b9c07a47835f73fc201af58394b9e8b69ba65
yokoffing <11689349+yokoffing@users.noreply.github.com>
2026-09-07 15:18:00 -0400

    disable nimbus

--- user.js
+++ user.js
@@ -10,7 +10,7 @@
 /****************************************************************************
  * Betterfox                                                                *
  * "Ad meliora"                                                             *
- * version: 153 ESR                                                         *
+ * version: 153.1 ESR                                                       *
  * url: https://github.com/yokoffing/Betterfox                              *
 ****************************************************************************/
 
@@ -142,6 +142,7 @@ user_pref("datareporting.usage.uploadEnabled", false);
 user_pref("app.shield.optoutstudies.enabled", false);
 user_pref("app.normandy.enabled", false);
 user_pref("app.normandy.api_url", "");
+user_pref("nimbus.rollouts.enabled", false);
 
 /** CRASH REPORTS ***/
 user_pref("breakpad.reportURL", "");
